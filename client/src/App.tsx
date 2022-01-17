8956bdda98aac54417a843288622ef303a6b2edf
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-01-17 10:25:23 +0100

    Oppsett av designsystem
    Co-authored-by: Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,39 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "@navikt/ds-css-internal";
+import "@navikt/ds-css";
+import { Alert } from "@navikt/ds-react";
+import { Dropdown, Divider } from "@navikt/ds-react-internal";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
           </button>
         </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+        <Alert variant="info">Designsystemet</Alert>
+        <Dropdown>
+          <Dropdown.Toggle>Toggle</Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Menu.GroupedList>
+              <Dropdown.Menu.GroupedList.Heading>
+                Systemer og oppslagsverk
+              </Dropdown.Menu.GroupedList.Heading>
+              <Dropdown.Menu.GroupedList.Item>Gosys</Dropdown.Menu.GroupedList.Item>
+            </Dropdown.Menu.GroupedList>
+            <Divider />
+            <Dropdown.Menu.List>
+              <Dropdown.Menu.List.Item>Gosys</Dropdown.Menu.List.Item>
+              <Dropdown.Menu.List.Item>Psys</Dropdown.Menu.List.Item>
+            </Dropdown.Menu.List>
+          </Dropdown.Menu>
+      </Dropdown>
     </div>
   )
 }
