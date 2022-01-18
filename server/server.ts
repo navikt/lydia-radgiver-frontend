6a8930b2cf9d2158ec146877a0e759278ea26630
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-01-18 17:11:17 +0100

    Håndter sigterm og steng serveren

--- server/server.ts
+++ server/server.ts
@@ -1,24 +1,33 @@
 import express from "express"
 import path from "path"
 import proxy from "./proxy";
+import http from 'http'
 
 const basePath = "/lydia-radgiver";
 const buildPath = path.resolve(__dirname, "../../client/dist");
-const server = express();
+const app = express();
 const port = process.env.PORT || 8080;
 
-server.use(basePath, express.static(buildPath));
-server.use("/assets", express.static(`${buildPath}/assets`));
+app.use(basePath, express.static(buildPath));
+app.use("/assets", express.static(`${buildPath}/assets`));
 
-server.get(`/internal/isAlive`, (req, res) => {
+app.get(`/internal/isAlive`, (req, res) => {
     res.sendStatus(200);
 });
 
-server.get(`/internal/isReady`, (req, res) => {
+app.get(`/internal/isReady`, (req, res) => {
     res.sendStatus(200);
 });
 
 // Proxy må ligge under healthcheck endepunktene for at de skal nås
-server.use("/api", proxy)
+app.use("/api", proxy)
 
-server.listen(port, () => console.log(`Server listening on port ${port}`));
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+process.on("SIGTERM", () => {
+    server.close()
+})
