d975dbe7e92e7cb6b086b933e70a5655806afb6b
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-01-18 11:36:52 +0100

    Lag en rewrite rule fra /api til / i proxy

--- server/proxy.ts
+++ server/proxy.ts
@@ -1,5 +1,8 @@
+import { Request } from "express";
 import { createProxyMiddleware } from "http-proxy-middleware"
 import { isNais } from "./env";
+
+const basePath = "/api"
 
 // Service name discovery: https://doc.nais.io/clusters/service-discovery/#service-discovery-in-kubernetes
 const targetURI = isNais ? "http://lydia-api" : "http://localhost:8080"
@@ -7,9 +10,12 @@ const targetURI = isNais ? "http://lydia-api" : "http://localhost:8080"
 const options = {
     target: targetURI,
     changeOrigin: true,
-    pathRewrite: {
-        '^/api': '/isAlive' // TODO: skriv denne om til en funksjon
+    pathRewrite: (path : string, req : Request) => {
+        const nyPath = path.replace(basePath, '');
+        console.log(`Proxy fra '${req.path}' til '${targetURI + nyPath}'`);
+        return nyPath;
     },
+
 };
 
 export default createProxyMiddleware(options);
