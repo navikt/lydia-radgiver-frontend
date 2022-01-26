63874c21ba647015b8041fe1110687f6547d859d
klechr <86653001+klechr@users.noreply.github.com>
2022-01-26 11:54:26 +0100

    Start på OnBehalfOf utveksling av tokens (#6)
    * Lagt til støtte for OnBehalfOf flow for å kalle backend api
    
    - lagt til axios for enklere http requests
    
    Co-authored-by: Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>

---
+++ server/onBehalfOf.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const tokenEndpoint = "TODO login microSoft"
+const clientId = "TODO loging microSoft"
+
+
+// OBO flyt som beskrevet her: 
+// https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-on-behalf-of-flow#first-case-access-token-request-with-a-shared-secret
+
+
+const config = {
+    clientId: process.env.AZURE_APP_CLIENT_ID,
+    tokenEndpoint: process.env.AZURE_OPENID_CONFIG_TOKEN_ENDPOINT,
+    clientSecret: process.env.AZURE_APP_CLIENT_SECRET,
+    lydiaApiScope: "api://lydia-api/.default"
+}
+
+
+export const hentOnBehalfOfToken = async (accessToken: string): Promise<String> => {
+    const options = {
+        grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+        client_id: config.clientId,
+        client_secret: config.clientSecret,
+        assertion: accessToken,
+        scope: config.lydiaApiScope,
+        requested_token_use: 'on_behalf_of',
+    }
+
+    // TODO sjekk at vi håndterer feil
+    return axios.post(config.tokenEndpoint, options)
+}
