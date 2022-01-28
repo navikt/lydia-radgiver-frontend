40fe33c11614b79eb6ea0cd275d5c15814c90fd5
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-01-28 10:03:15 +0100

    Tester at endepunkter for liveness og readiness fungerer
    Co-authored-by: Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>

---
+++ server/app.test.ts
@@ -0,0 +1,21 @@
+import request from "supertest"
+import app from "./app"
+
+describe("Tester liveness og readiness", () => {
+    test("Appen skal respondere på liveness", done => {
+      request(app)
+          .get("/internal/isAlive")
+          .then(response => {
+              expect(response.statusCode).toBeLessThan(400);
+              done();
+          });
+    });
+    test("Appen skal respondere på readiness", done => {
+      request(app)
+          .get("/internal/isReady")
+          .then(response => {
+              expect(response.statusCode).toBeLessThan(400);
+              done();
+          });
+    });
+});
--- server/server.test.ts
+++
@@ -1,3 +0,0 @@
-test("Dummy unit test", () => {
-  expect(1 + 2).toBe(3);
-});
