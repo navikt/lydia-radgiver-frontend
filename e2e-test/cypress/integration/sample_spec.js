a90d0570535b33482991b26c03b73edb85491696
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-01-31 15:01:14 +0100

    Tester at lydia-radigiver-frontend når lydia-api
    Co-authored-by: Stefan Andreas Engelien <stefan.andreas.engelien@nav.no>

--- e2e-test/cypress/integration/sample_spec.js
+++ e2e-test/cypress/integration/sample_spec.js
@@ -1,15 +1,10 @@
-// describe('', () => {
-//     beforeEach(() => {
-//       cy.visit("http://localhost:9876/lydia-radgiver");
-//     })
-// })
-
-
-describe('My First Test', () => {
-    beforeEach(() => {
-      cy.visit("http://localhost:9876/lydia-radgiver");
-    });
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
+describe('Tester at lydia-radigiver-frontend når lydia-api', () => {
+    it('Forventer 401 når man kaller /api uten gyldig token', () => {
+        const endepunkt = "/api/test"
+        cy.intercept(endepunkt, (req) => {
+            req.headers['authorization'] = 'bearer my-bearer-auth-token'
+        }).as('addAuthHeader')
+        cy.visit(`http://localhost:9876${endepunkt}`, { failOnStatusCode: false });
+        cy.wait('@addAuthHeader').its("response.statusCode").should("equal", 401)
     })
-  })
+})
