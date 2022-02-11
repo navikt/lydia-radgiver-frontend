761962a8fb4bca1e3e03c6e0346b72148aea18f7
Ulrik Antoniussen Halmøy <ulrikhalmoy@gmail.com>
2022-02-11 09:53:00 +0100

    Gjør det mulig å bygge lokalt uten å installere avhengigheter [skip ci]
    Bruk ./run.sh --no-install

--- run.sh
+++ run.sh
@@ -1,18 +1,32 @@
 #! /usr/bin/env bash
 
-
-echo "Bygger prosjektet lokalt"
-
 ENV_FILE=env.local
 LOCAL_PORT=9876
 
-cd client
-npm install
-npm run build
-cd ../server
-npm install
-npm run build
-cd ..
-docker build -t lydia-radgiver-frontend:latest .
-docker run -p $LOCAL_PORT:8080 --env-file $ENV_FILE -d lydia-radgiver-frontend:latest
-echo "lydia-radgiver-frontend er tilgjengelig på port http://localhost:$LOCAL_PORT/lydia-radgiver"
+function install_deps(){
+    echo "Installerer avhengigheter"
+    cd client
+    npm install
+    npm run build
+    cd ../server
+    npm install
+    npm run build
+    cd ..    
+}
+
+function build_local(){
+    echo "Bygger prosjektet lokalt"
+    docker build -t lydia-radgiver-frontend:latest .
+    docker run -p $LOCAL_PORT:8080 --env-file $ENV_FILE -d lydia-radgiver-frontend:latest
+    echo "lydia-radgiver-frontend er tilgjengelig på port http://localhost:$LOCAL_PORT/lydia-radgiver"
+}
+
+if [ "$1" == "--no-install" ]; then
+    build_local
+else
+    install_deps
+    build_local
+fi
+
+unset install_deps
+unset build_local
