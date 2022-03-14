c7465ca048a642a88ec309503d28a970f7c4d8b8
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-03-14 15:15:22 +0100

    Legg til sorteringsmuligheter som en søkeparameter-dropdown

--- client/src/Pages/Prioritering/filterverdierMock.tsx
+++ client/src/Pages/Prioritering/filterverdierMock.tsx
@@ -1,48 +1,49 @@
 import { Filterverdier } from "../../domenetyper";
 
 export const filterverdierMock: Filterverdier = {
-    fylker: [
+  fylker: [
+    {
+      fylke: {
+        nummer: "34",
+        navn: "Innlandet",
+      },
+      kommuner: [
         {
-            fylke: {
-                nummer: "34",
-                navn: "Innlandet",
-            },
-            kommuner: [
-                {
-                    navn: "Nord-Aurdal",
-                    nummer: "3451"
-                },
-                {
-                    navn: "Vestre Slidre",
-                    nummer: "3452"
-                },
-                {
-                    navn: "Øystre Slidre",
-                    nummer: "3453"
-                },
-            ],
+          navn: "Nord-Aurdal",
+          nummer: "3451",
         },
         {
-            fylke: {
-                nummer: "03",
-                navn: "Oslo",
-            },
-            kommuner: [
-                {
-                    navn: "Oslo",
-                    nummer: "0301"
-                },
-            ],
-        },
-    ],
-    næringsgrupper: [
-        {
-            kode : "01",
-            navn : "A - Jordbruk og tjenester tilknyttet jordbruk, jakt og viltstell",
+          navn: "Vestre Slidre",
+          nummer: "3452",
         },
         {
-            kode : "02",
-            navn: "B - Skogbruk og tjenester tilknyttet skogbruk"
-        }
-    ]
-}
+          navn: "Øystre Slidre",
+          nummer: "3453",
+        },
+      ],
+    },
+    {
+      fylke: {
+        nummer: "03",
+        navn: "Oslo",
+      },
+      kommuner: [
+        {
+          navn: "Oslo",
+          nummer: "0301",
+        },
+      ],
+    },
+  ],
+  næringsgrupper: [
+    {
+      kode: "01",
+      navn: "A - Jordbruk og tjenester tilknyttet jordbruk, jakt og viltstell",
+    },
+    {
+      kode: "02",
+      navn: "B - Skogbruk og tjenester tilknyttet skogbruk",
+    },
+  ],
+  sorteringsnøkler: ["tapte_dagsverk", "sykefraversprosent"],
+};
