19743acba3f51e9584c15a33959c926c5183bd74
Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
2022-03-16 13:52:54 +0100

    Bruk knapp fra designsystemet i stedet for default button for søk

--- client/src/Pages/Prioritering/Prioriteringsside.tsx
+++ client/src/Pages/Prioritering/Prioriteringsside.tsx
@@ -10,56 +10,64 @@ import {
   Filterverdier,
   SykefraversstatistikkVirksomhet,
 } from "../../domenetyper";
+import { Button } from "@navikt/ds-react";
 
 const Prioriteringsside = () => {
-  const [søkeverdier, setSøkeverdier] = useState<Søkeverdier>();
-  const [skalSøke, setSkalSøke] = useState(false)
-  const {
-    data: filterverdier,
-    loading: loadingFilterverdier,
-    error: errorFilterverdier,
-  } = useFilterverdier();
-  const {
-    data: sykefraversstatistikk,
-    loading: loadingSykefraværsstatistikk,
-    error: errorSykefraværsstatistikk,
-  } = useSykefraværsstatistikk({ søkeverdier, initierSøk: skalSøke });
+    const [søkeverdier, setSøkeverdier] = useState<Søkeverdier>();
+    const [skalSøke, setSkalSøke] = useState(false);
+    const {
+        data: filterverdier,
+        loading: loadingFilterverdier,
+        error: errorFilterverdier,
+    } = useFilterverdier();
+    const {
+        data: sykefraversstatistikk,
+        loading: loadingSykefraværsstatistikk,
+        error: errorSykefraværsstatistikk,
+    } = useSykefraværsstatistikk({ søkeverdier, initierSøk: skalSøke });
 
-  useEffect(() => {
-    if (skalSøke) {
-      setSkalSøke(false)
-    }
-  }, [skalSøke])
-
-  const isLoading = loadingFilterverdier || loadingSykefraværsstatistikk;
-  const isError = errorFilterverdier || errorSykefraværsstatistikk;
-
-  const gjørNyttSøk = (nyeSøkeverdier: Søkeverdier) => {
-    setSøkeverdier({ ...søkeverdier, ...nyeSøkeverdier });
-  };
-  const tommeFilterverdier: Filterverdier = {
-      fylker: [],
-      neringsgrupper: [],
-      sorteringsnokler: [],
-  };
-  const tomSykefraværsstatistikk: SykefraversstatistikkVirksomhet[] = [];
-
-  return (
-    <>
-      <Filtervisning
-        filterverdier={filterverdier ?? tommeFilterverdier}
-        oppdaterSøkeverdier={gjørNyttSøk}
-      />
-      <PrioriteringsTabell
-        sykefraværsstatistikk={
-          sykefraversstatistikk ?? tomSykefraværsstatistikk
+    useEffect(() => {
+        if (skalSøke) {
+            setSkalSøke(false);
         }
-      />
-      <button onClick={() => {setSkalSøke(true)}}>Søk</button>
-      {isLoading && <p>Loading</p>}
-      {isError && <p>Error</p>}
-    </>
-  );
+    }, [skalSøke]);
+
+    const isLoading = loadingFilterverdier || loadingSykefraværsstatistikk;
+    const isError = errorFilterverdier || errorSykefraværsstatistikk;
+
+    const gjørNyttSøk = (nyeSøkeverdier: Søkeverdier) => {
+        setSøkeverdier({ ...søkeverdier, ...nyeSøkeverdier });
+    };
+    const tommeFilterverdier: Filterverdier = {
+        fylker: [],
+        neringsgrupper: [],
+        sorteringsnokler: [],
+    };
+    const tomSykefraværsstatistikk: SykefraversstatistikkVirksomhet[] = [];
+
+    return (
+        <>
+            <Filtervisning
+                filterverdier={filterverdier ?? tommeFilterverdier}
+                oppdaterSøkeverdier={gjørNyttSøk}
+            />
+            <Button
+                size="small"
+                onClick={() => {
+                    setSkalSøke(true);
+                }}
+            >
+                Søk
+            </Button>
+            <PrioriteringsTabell
+                sykefraværsstatistikk={
+                    sykefraversstatistikk ?? tomSykefraværsstatistikk
+                }
+            />
+            {isLoading && <p>Loading</p>}
+            {isError && <p>Error</p>}
+        </>
+    );
 };
 
 export default Prioriteringsside;
