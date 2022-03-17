525b7da87846a94b1c6c1020b3b084ffbcf0124a
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-03-17 10:04:55 +0100

    Legg til manglende prop for søkpånytt

--- client/src/Pages/Prioritering/Filtervisning.stories.tsx
+++ client/src/Pages/Prioritering/Filtervisning.stories.tsx
@@ -1,17 +1,19 @@
-import { ComponentMeta } from "@storybook/react";
+import {ComponentMeta} from "@storybook/react";
 import Filtervisning from "./Filtervisning";
-import { filterverdierMock } from "./filterverdierMock";
+import {filterverdierMock} from "./filterverdierMock";
 import "@navikt/ds-css";
 
 export default {
-  title: "Filtervisning",
-  component: Filtervisning,
+    title: "Filtervisning",
+    component: Filtervisning,
 } as ComponentMeta<typeof Filtervisning>;
 
 export const Hovedstory = () => (
-  <Filtervisning
-    filterverdier={filterverdierMock}
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    oppdaterSøkeverdier={() => {}}
-  />
+    <Filtervisning
+        søkPåNytt={() => {}}
+        filterverdier={filterverdierMock}
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        oppdaterSøkeverdier={() => {
+        }}
+    />
 );
