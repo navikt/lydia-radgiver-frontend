a75a1e95010aae231237ffd2098212ab98b5cc61
Tanet Trimas <tanet@trimas.no>
2022-03-23 15:08:14 +0100

    Legg til en test for kryptering

---
+++ server/__tests__/crypto.test.ts
@@ -0,0 +1,17 @@
+import { decrypt, encrypt } from "../crypto";
+
+describe("crypto", () => {
+    process.env.OBO_TOKEN_ENC_KEY = "Laila";
+    it("should encrypt and decrypt properly", async () => {
+        const message = "TIL VALHALL!!";
+        const [encryptedFirst, encryptedSecond] = await Promise.all([
+            encrypt(message),
+            encrypt(message),
+        ]);
+        expect(encryptedFirst).not.toBe(encryptedSecond);
+        const allDecryptToSame = await (
+            await Promise.all([decrypt(encryptedFirst), decrypt(encryptedFirst)])
+        ).every((decrypted) => decrypted === message);
+        expect(allDecryptToSame).toBeTruthy();
+    });
+});
