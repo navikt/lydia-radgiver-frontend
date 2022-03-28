081840dc943760ddcb45c263a90b4805333e2c18
Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
2022-03-28 10:27:03 +0200

    Tar i bruk prometheus-api-metrics biblioteket for å logge API-relaterte Prometheus-metrikker

--- server/metrikker.ts
+++
@@ -1,7 +0,0 @@
-import client, {Registry} from "prom-client";
-
-export const konfigurerMetrikker = () => {
-    const register = new Registry();
-    client.collectDefaultMetrics({ register });
-    return register;
-}
