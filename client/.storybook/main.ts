8b1e0bd6c723d641767fb8714b62df7029280ed1
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-04-20 09:38:32 +0200

    Oppgrader til react 18 (#37)
    * Oppgrader til react 18
    
    * Gjjør slik at npm ikke stopper ved installasjon av peerdeps
    
    React 18 skal fungere fint på både storybook og appen, og skal ikke være avhengig av at alle transitive deps også må støtte react 18
    
    * Fjern todo
    
    * Legg legacy-peer-deps i riktig mappe
    
    * Oppgraderer deps, samt nedgraderer fra alpha dep av storybook til en mer stabil versjon
    
    * Legg til package-lock, regenerert

---
+++ client/.storybook/main.ts
@@ -0,0 +1,17 @@
+module.exports = {
+  "stories": ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+
+  async viteFinal(config, {
+    configType
+  }) {
+    config.base = process.env.BASE_URL || config.base; // return the customized config
+
+    return config;
+  }
+
+};
