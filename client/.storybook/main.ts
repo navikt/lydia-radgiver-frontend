9260fac3b863bd469f8f906d92c86eb0cb59241c
Tanet Trimas <tanet@trimas.no>
2022-04-20 11:32:02 +0200

    Flytt public inn til storybook mappen

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,17 +1,14 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  },
+    stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+    framework: "@storybook/react",
+    core: {
+        builder: "@storybook/builder-vite",
+    },
+    staticDirs: ["public"],
+    async viteFinal(config, { configType }) {
+        config.base = process.env.BASE_URL || config.base; // return the customized config
 
-  async viteFinal(config, {
-    configType
-  }) {
-    config.base = process.env.BASE_URL || config.base; // return the customized config
-
-    return config;
-  }
-
+        return config;
+    },
 };
