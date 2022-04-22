0e94ea8458e627fe1e2558f8103d9ecaf1df1cec
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-04-22 14:26:19 +0200

    Legg til mulighet til å ha queryparams i en story

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,6 +1,10 @@
 module.exports = {
     stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-queryparams",
+    ],
     framework: "@storybook/react",
     core: {
         builder: "@storybook/builder-vite",
