c4a138e2d794fd529e68be07f91d92bf2eed1df0
Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
2022-05-10 13:05:10 +0200

    Legger til informasjon om når brukeren sitt token utløper i respons fra frackend

--- server/brukerinfo.ts
+++ server/brukerinfo.ts
@@ -1,19 +1,34 @@
-import { NextFunction, Request, Response } from "express";
-import { decodeJwt } from "jose";
-import { AuthError } from "./error";
-import { getBearerToken } from "./onBehalfOf";
+import {NextFunction, Request, Response} from "express";
+import {decodeJwt, JWTPayload} from "jose";
+import {AuthError} from "./error";
+import {getBearerToken} from "./onBehalfOf";
 
-type NavAnsatt = {
+export type Brukerinformasjon = {
     navn: string;
     ident: string;
     epost: string;
+    tokenUtløper: number
 };
 
-const lokalMockBruker: NavAnsatt = {
+const lokalMockBruker: Brukerinformasjon = {
     navn: "Ansatt, Lokal",
     ident: "A123456",
     epost: "lokal.ansatt@nav.no",
+    tokenUtløper: Date.now() + 5000
 };
+
+export const hentBrukerinfoFraToken = (jwtPayload : JWTPayload) : Brukerinformasjon => {
+    const navn = jwtPayload["name"] as string;
+    const ident = jwtPayload["NAVident"] as string;
+    const preferredUsername = jwtPayload["preferred_username"] as string;
+    const tokenUtløper = jwtPayload.exp * 1000 // konverterer fra sekunder til ms
+    return {
+        navn: navn,
+        ident: ident,
+        epost: preferredUsername,
+        tokenUtløper: tokenUtløper
+    }
+}
 
 export const hentInnloggetAnsattMiddleware = (
     req: Request,
@@ -27,15 +42,7 @@ export const hentInnloggetAnsattMiddleware = (
         if (!bearerToken) {
             return next(new AuthError("Mangler token i auth header"));
         }
-        const claims = decodeJwt(bearerToken);
-        const navn = claims["name"] as string;
-        const ident = claims["NAVident"] as string;
-        const preferredUsername = claims["preferred_username"] as string;
-        const navAnsatt: NavAnsatt = {
-            navn: navn,
-            ident: ident,
-            epost: preferredUsername,
-        };
-        return res.send(navAnsatt);
+        const jwtPayload = decodeJwt(bearerToken);
+        return res.send(hentBrukerinfoFraToken(jwtPayload));
     }
 };
