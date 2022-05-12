2b3f6a8af8fe0a6cd1267a832d53af89eebd6890
Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
2022-05-12 09:31:31 +0200

    Renskriving av hjelpetekst før man velger begrunnelser etter input fra teamet

--- client/src/Pages/Virksomhet/BegrunnelseModal.tsx
+++ client/src/Pages/Virksomhet/BegrunnelseModal.tsx
@@ -31,7 +31,7 @@ export const ModalInnhold = ({hendelse, lagre}: Props & { lagre: (valgtÅrsak: V
     return (
         <>
             <Select
-                label="Begrunnelse for at samarbeid ikke blir startet:"
+                label="Begrunnelse for at samarbeid ikke er aktuelt:"
                 onChange={(e) => {
                     setValgtÅrsak(hentÅrsakFraÅrsakType(e.target.value, hendelse));
                     setValgteBegrunnelser([]);
