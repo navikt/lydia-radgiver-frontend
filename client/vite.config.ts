1a55d1f6271e343f8be46ac2dd8bd8f691b05dfd
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-05-18 14:34:32 +0200

    Legg til inspect plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import Inspect from "vite-plugin-inspect";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), Inspect()],
     server: {
         host: true,
         proxy: {
