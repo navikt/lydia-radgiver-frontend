757f660022fa861a6027337969204db32f8ec9a2
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-05-19 08:52:46 +0200

    Legg til sri plugin (#46)
    * Legg til inspect plugin
    
    * Legg til subresource integrity på script og link tags

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,20 +1,24 @@
 import { defineConfig } from "vite";
+import Inspect from "vite-plugin-inspect";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        host: true,
-        proxy: {
-            "/api": {
-                target: "http://localhost:8080",
-                changeOrigin: true,
+export default defineConfig(async () => {
+    const sri = await import("@small-tech/vite-plugin-sri");
+    return {
+        plugins: [react(), Inspect(), sri.default()],
+        server: {
+            host: true,
+            proxy: {
+                "/api": {
+                    target: "http://localhost:8080",
+                    changeOrigin: true,
+                },
+                "/innloggetAnsatt": "http://localhost:8080",
             },
-            "/innloggetAnsatt": "http://localhost:8080",
         },
-    },
-    build: {
-        sourcemap: true,
-    },
+        build: {
+            sourcemap: true,
+        },
+    };
 });
