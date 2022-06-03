d29187dd515c1cc9170f0b4bf8c7b44cb66be79f
Ulrik Antoniussen Halmøy <25346380+ulrikah@users.noreply.github.com>
2022-06-03 14:03:17 +0200

    Lar bruker filtrere på kun sine virksomheter (#48)
    * MVP filtervisning for å huke av for 'mine virksomheter'
    
    * Plasserer søkeknappen lengst mot høyre

--- client/src/Pages/Prioritering/Filtervisning.tsx
+++ client/src/Pages/Prioritering/Filtervisning.tsx
@@ -19,6 +19,7 @@ import styled from "styled-components";
 import {hvitRammeMedBoxShadow} from "../../styling/containere";
 import {Kommunedropdown} from "./Kommunedropdown";
 import {AntallAnsatteVelger} from "./AntallAnsatteVelger";
+import {KunMineVirksomheterToggle} from "./KunMineVirksomheterToggle";
 
 export const sorteringsverdier = {
     tapte_dagsverk: "Tapte dagsverk",
@@ -36,6 +37,7 @@ interface FiltervisningProps {
 
 const Søkeknapp = styled(Button)`
     width: 10rem;
+    margin-left: auto;
 `;
 
 const næringsgruppeKoderTilNæringsgrupper = (
@@ -202,6 +204,9 @@ const Filtervisning = ({
                     statuser={filterverdier.statuser}
                     valgtStatus={IAStatus}
                 />
+                <KunMineVirksomheterToggle onChangeCallback={(visKunMineVirksomheter) =>
+                    oppdaterSøkeverdier({ kunMineVirksomheter : visKunMineVirksomheter})
+                }/>
                 <Søkeknapp
                     size="medium"
                     onClick={() => {
