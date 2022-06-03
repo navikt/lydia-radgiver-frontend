0e4a3576d783601e115c1114ce8d1714a1767128
Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
2022-06-03 11:08:47 +0200

    WIP: checkbox i stedet for toggle group for å vise mine virksomheter

--- client/src/Pages/Prioritering/KunMineVirksomheterToggle.tsx
+++ client/src/Pages/Prioritering/KunMineVirksomheterToggle.tsx
@@ -1,4 +1,4 @@
-import {ToggleGroup} from "@navikt/ds-react";
+import {CheckboxGroup, Checkbox, ToggleGroup} from "@navikt/ds-react";
 import {useState} from "react";
 
 interface Props {
@@ -13,11 +13,35 @@ const skalViseKunMineVirksomheter = (toggleVerdi : string) =>
     toggleVerdi === toggleVerdier.visKunMineVirksomheter
 
 export const KunMineVirksomheterToggle = ({ onChangeCallback } : Props) => {
+    return CheckboxVisKunMineVirksomheter() // Toggle();
+}
+
+function CheckboxVisKunMineVirksomheter() {
+    const checkedState = ["visKunMineVirksomheter"]
+    const [visKunMineVirksomheter, setVisKunMineVirksomheter] = useState<boolean>(false)
+    return <CheckboxGroup
+        onChange={(nyToggleVerdi : string[]) => {
+            console.log(nyToggleVerdi)
+            setVisKunMineVirksomheter(nyToggleVerdi in checkedState)
+            // onChangeCallback(skalViseKunMineVirksomheter(nyToggleVerdi))
+        }}
+        hideLegend={true}
+        legend={"Vis kun mine virksomheter"}
+        size="small"
+        value={visKunMineVirksomheter ? checkedState : []}
+    >
+        <Checkbox value={checkedState}>
+            Vis kun mine virksomheter
+        </Checkbox>
+    </CheckboxGroup>
+}
+
+function Toggle() {
     const [toggleVerdi, setToggleVerdi] = useState<string>(toggleVerdier.visAlleVirksomheter)
     return <ToggleGroup
         onChange={(nyToggleVerdi) => {
             setToggleVerdi(nyToggleVerdi)
-            onChangeCallback(skalViseKunMineVirksomheter(nyToggleVerdi))
+            // onChangeCallback(skalViseKunMineVirksomheter(nyToggleVerdi))
         }}
         size="small"
         value={toggleVerdi}
@@ -30,5 +54,6 @@ export const KunMineVirksomheterToggle = ({ onChangeCallback } : Props) => {
         </ToggleGroup.Item>
     </ToggleGroup>
 }
+
 
 
