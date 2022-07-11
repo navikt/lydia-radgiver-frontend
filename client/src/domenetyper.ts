6eca4f124b82ddff1c9fd412f830c2d8133e856e
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-07-11 09:54:07 +0200

    Bruk nullable i stedet

--- client/src/domenetyper.ts
+++ client/src/domenetyper.ts
@@ -55,7 +55,7 @@ export const sykefraversstatistikkVirksomhetListeSchema = z.array(
 
 export const sykefraværListeResponsSchema = z.object({
     data: sykefraversstatistikkVirksomhetListeSchema,
-    total: z.number().optional()
+    total: z.number().nullable()
 })
 
 export const filterverdierSchema = z.object({
