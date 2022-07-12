bf4a2ab9e99b865568b7067ced182f7bec5325bb
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-07-12 14:53:27 +0200

    Tøm resultatene når man trykker på et firma
    Co-authored-by: Stefan Andreas Engelien <stefan.andreas.engelien@nav.no>

--- client/src/components/Dekoratør/Søkefelt.tsx
+++ client/src/components/Dekoratør/Søkefelt.tsx
@@ -1,9 +1,9 @@
 import {Link, Popover, Search} from "@navikt/ds-react";
 import {CSSProperties, useEffect, useRef, useState} from "react";
-import {VirksomhetSøkeresultat} from "../../domenetyper";
 import {useDebounce} from "../../util/useDebounce";
 import {virksomhetAutocompletePath} from "../../api/lydia-api";
 import { Link as RouterLink } from "react-router-dom";
+import {VirksomhetSøkeresultat} from "../../domenetyper";
 
 interface Props {
     darkmode?: boolean,
@@ -47,6 +47,10 @@ export const Søkefelt = ({ darkmode, style }: Props) => {
                         style={{display: "block"}}
                         as={RouterLink}
                         to={`/virksomhet/${firma.orgnr}`}
+                        onClick={() => {
+                            setSøkestreng("")
+                            setFirmaer([])
+                        }}
                     >
                         {firma.navn}
                     </Link>
