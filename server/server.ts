97dd077b82d7d873b674bc12d9fe4d0a43da860c
Christian Klem <christian.klem@nav.no>
2022-08-29 14:35:31 +0200

    Spinn egen express server i labs-miljø
    Co-authored-by: Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>

--- server/server.ts
+++ server/server.ts
@@ -1,35 +1,65 @@
 import http from 'http'
 import Application from './app';
-import { Config } from './config';
+import {Config, Server} from './config';
 import logger from "./logging"
 import { setupRemoteJwkSet } from "./jwks";
 import dotenv from "dotenv"
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as types from "./types"; // nødvendig for at ts-node skal skjønne at disse typene eksisterer
+import express, {Express} from "express";
+import path from "path"; // nødvendig for at ts-node skal skjønne at disse typene eksisterer
 
+interface AppProvider {
+    application: Express,
+    serverConfig: Server
+}
+
+
+const appProvider = async (): Promise<AppProvider> => {
+    if (process.env.NAIS_CLUSTER_NAME === "labs-gcp") {
+        const application = express()
+        application.get(
+            ["/internal/isAlive", "/internal/isReady"],
+            (req, res) => {
+                res.sendStatus(200);
+            }
+        );
+        const buildPath = path.resolve(__dirname, "../client/dist");
+        application.get("/assets", express.static(`${buildPath}/assets`));
+        application.use("/", express.static(buildPath));
+        application.use("/*", express.static(buildPath));
+        return Promise.resolve({application, serverConfig: new Server()})
+    } else {
+        return setupRemoteJwkSet()
+            .then(jwkSet => {
+                const config = new Config({jwkSet})
+                const application = new Application(config)
+                return {application: application.expressApp, serverConfig: config.server}
+            })
+    }
+}
 
 const main = () => {
     dotenv.config({ path: "../env.local"})
-    setupRemoteJwkSet()
-        .then(jwkSet => {
-            const config = new Config({jwkSet})
-            const application = new Application(config)
-            const server = http.createServer(application.expressApp);
+    appProvider().then(appProvider => {
+        const server = http.createServer(appProvider.application);
 
-            const gracefulClose = () => {
-                server.close(() => {
-                    logger.info("App shutting down...")
-                    process.exit(0)
-                })
-            }
+        const gracefulClose = () => {
+            server.close(() => {
+                logger.info("App shutting down...")
+                process.exit(0)
+            })
+        }
 
-            server.listen(config.server.port, () => {
-                logger.info(`Server listening on port ${config.server.port}`);
-            });
+        server.listen(appProvider.serverConfig.port, () => {
+            logger.info(`Server listening on port ${appProvider.serverConfig.port}`);
+        });
 
-            process.on("SIGTERM", gracefulClose)
-            process.on("SIGINT", gracefulClose)
-        })
+        process.on("SIGTERM", gracefulClose)
+        process.on("SIGINT", gracefulClose)
+    })
+
+
+
 }
 
 main()
