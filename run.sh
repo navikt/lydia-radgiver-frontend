ae44bd1a58ed96b0b32ef3ec866fd8d3cf1fe3ca
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-09-29 13:07:19 +0200

    Run scriptet henter ned ny dump med testdata

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env bash
 
 docker-compose up -d
+curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia_api_container_db_localhost-2022_09_29_10_21_57-dump.sql
+sleep 3
+PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f db_script.sql
+rm db_script.sql
 cd client
 npm install
 npm run dev
