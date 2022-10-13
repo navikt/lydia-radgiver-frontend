58e37ca0adc8d507c6303661bfbf1387569081f7
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-10-13 10:26:16 +0200

    Oppdatert tekster
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- client/src/Pages/Virksomhet/SakshendelsesKnapper.tsx
+++ client/src/Pages/Virksomhet/SakshendelsesKnapper.tsx
@@ -40,10 +40,12 @@ const beskrivelseForHendelse = ({hendelse, sakstatus}: {
     const penskrevetHendelse = penskrivIASakshendelsestype(hendelse.saksHendelsestype)
     switch (hendelse.saksHendelsestype) {
         case "FULLFØR_BISTAND":
-            return `Du er i ferd med å fullføre den tidsbegrensede avtalen med denne virksomheten. Er dette riktig?`
+            return `Du har valgt hendelsen "Fullfør" – velges når avtalt IA-oppfølging er fullført. Saken lukkes.`
         case "TILBAKE": {
-            if (sakstatus === IAProsessStatusEnum.enum.FULLFØRT) return `Hvis du velger "${penskrevetHendelse}" vil saken gjenåpnes og få status "${penskrivIAStatus(IAProsessStatusEnum.enum.VI_BISTÅR)}".`
-            return `Du har valgt hendelsen "${penskrevetHendelse}"`;
+            let text = `Du har valgt hendelsen "${penskrevetHendelse}"`
+            if (sakstatus === IAProsessStatusEnum.enum.FULLFØRT) text += ` - velges når du vil gjenåpne saken og gå tilbake til status "${penskrivIAStatus(IAProsessStatusEnum.enum.VI_BISTÅR)}".`
+            if (sakstatus === IAProsessStatusEnum.enum.IKKE_AKTUELL) text += ` - velges når du vil gjenåpne saken og gå tilbake til siste status.`
+            return text;
         }
         default:
             return ""
