3ee07dc9b865a774cb7c77b06f43742ab5556b3f
Ingrid Fosså <fossa.ingrid@gmail.com>
2022-10-17 16:47:46 +0200

    Legg til storybook for paginering

---
+++ client/src/Pages/Prioritering/Paginering.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta } from "@storybook/react";
+import { Paginering } from "./Paginering";
+import { useState } from "react";
+import { ANTALL_RESULTATER_PER_SIDE } from "./Prioriteringsside";
+
+export default {
+    title: "Prioritering/Paginering",
+    component: Paginering,
+} as ComponentMeta<typeof Paginering>;
+
+export const Hovedstory = () => {
+    const [side, setSide] = useState(1);
+    const antallTreffPåSide = side === 5 ? 10 : ANTALL_RESULTATER_PER_SIDE;
+
+    const endreSide = (side: number) => {
+        setSide(side)
+    }
+
+    return <Paginering side={side} endreSide={endreSide} antallTreffPåSide={antallTreffPåSide} />
+}
