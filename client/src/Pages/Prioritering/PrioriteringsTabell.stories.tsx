ce30b5a64bc53b65288c74a0e03aaa96b5a24857
Ingrid Fosså <fossa.ingrid@gmail.com>
2022-10-17 16:49:38 +0200

    Fjernar ugyldig prop i story for prioriteringstabell

--- client/src/Pages/Prioritering/PrioriteringsTabell.stories.tsx
+++ client/src/Pages/Prioritering/PrioriteringsTabell.stories.tsx
@@ -1,24 +1,23 @@
-import {ComponentMeta} from "@storybook/react";
-import {StyledPrioriteringsTabell} from "./PrioriteringsTabell";
-import {sykefraværsstatistikkMock} from "./mocks/sykefraværsstatistikkMock";
-import {useState} from "react";
-import {SortState} from "@navikt/ds-react";
+import { ComponentMeta } from "@storybook/react";
+import { StyledPrioriteringsTabell } from "./PrioriteringsTabell";
+import { sykefraværsstatistikkMock } from "./mocks/sykefraværsstatistikkMock";
+import { useState } from "react";
+import { SortState } from "@navikt/ds-react";
 
 export default {
     title: "Prioritering/Prioriteringstabell",
     component: StyledPrioriteringsTabell,
 } as ComponentMeta<typeof StyledPrioriteringsTabell>;
 
-function repeatArray<T>(arr : T[], repeats : number) : T[] {
-    return Array.from({ length: repeats }, () => arr).flat();
+function repeatArray<T>(arr: T[], repeats: number): T[] {
+    return Array.from({length: repeats}, () => arr).flat();
 }
 
 export const Hovedstory = () => {
     const sykefraværsstatistikk = repeatArray(sykefraværsstatistikkMock, 5)
     const antallPerSide = 5
-    const totaltAntallResultaterISøk = sykefraværsstatistikk.length
     const [side, setSide] = useState(1)
-    const [sortering, setSortering] = useState<SortState>({ direction: "descending", orderBy: "tapte_dagsverk" })
+    const [sortering, setSortering] = useState<SortState>({direction: "descending", orderBy: "tapte_dagsverk"})
 
     return (
         <StyledPrioriteringsTabell
@@ -32,7 +31,6 @@ export const Hovedstory = () => {
                 setSortering(sortering)
                 setSide(1)
             })}
-            totaltAntallResultaterISøk={totaltAntallResultaterISøk}
         />
     );
 };
