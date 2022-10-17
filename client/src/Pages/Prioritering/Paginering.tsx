2ba951b222175a5e3815272888fd10628e7d5238
Ingrid Fosså <fossa.ingrid@gmail.com>
2022-10-17 16:32:37 +0200

    Ryddar litt i styling

--- client/src/Pages/Prioritering/Paginering.tsx
+++ client/src/Pages/Prioritering/Paginering.tsx
@@ -1,10 +1,17 @@
-import Item from "@navikt/ds-react/esm/pagination/PaginationItem";
-import cl from "clsx";
-import {Back, Next} from "@navikt/ds-icons";
-import {BodyShort} from "@navikt/ds-react/src";
-import {ANTALL_RESULTATER_PER_SIDE} from "./Prioriteringsside";
-import {Detail} from "@navikt/ds-react";
 import React from "react";
+import styled from "styled-components";
+import cl from "clsx";
+import Item from "@navikt/ds-react/esm/pagination/PaginationItem";
+import { Back, Next } from "@navikt/ds-icons";
+import { BodyShort } from "@navikt/ds-react/src";
+import { Detail } from "@navikt/ds-react";
+import { ANTALL_RESULTATER_PER_SIDE } from "./Prioriteringsside";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10rem;
+`;
 
 interface Props {
     side: number;
@@ -13,57 +20,56 @@ interface Props {
 }
 
 export const Paginering = ({side, antallTreffPåSide, endreSide}: Props) => {
-    return (<div style={{display: "flex", alignItems: "center", flexDirection: "row", gap: "10rem"}}>
-        <Item
-            className={cl("navds-pagination__prev-next", {
-                "navds-pagination--invisible": side === 1,
-                "navds-pagination--prev-next--with-text": true,
-            })}
-            disabled={side <= 1}
-            onClick={() => endreSide?.(side - 1)}
-            page={side - 1}
-            size={"small"}
-            icon={
-                <Back
-                    className="navds-pagination__prev-next-icon"
-                    aria-hidden={true}
-                />
-            }
-        >
-            <BodyShort
-                size={"small"}
-                className="navds-pagination__prev-text"
+    return (
+        <Container>
+            <Item
+                className={cl("navds-pagination__prev-next", "navds-pagination--prev-next--with-text", {
+                    "navds-pagination--invisible": side === 1
+                })}
+                disabled={side <= 1}
+                onClick={() => endreSide?.(side - 1)}
+                page={side - 1}
+                size="small"
+                icon={
+                    <Back
+                        className="navds-pagination__prev-next-icon"
+                        aria-hidden={true}
+                    />
+                }
             >
-                Forrige
-            </BodyShort>
-        </Item>
-        <BodyShort>{side}</BodyShort>
-        <Item
-            className={cl("navds-pagination__prev-next", {
-                "navds-pagination--invisible": antallTreffPåSide !== ANTALL_RESULTATER_PER_SIDE,
-                "navds-pagination--prev-next--with-text": true,
-            })}
-            disabled={antallTreffPåSide !== ANTALL_RESULTATER_PER_SIDE}
-            onClick={() => endreSide?.(side + 1)}
-            page={side + 1}
-            size={"small"}
-            icon={
-                <Next
-                    className="navds-pagination__prev-next-icon"
-                    aria-hidden={true}
-                />
-            }
-            iconPosition="right"
-        >
-            <BodyShort
-                size={"small"}
-                className="navds-pagination__next-text"
+                <BodyShort
+                    size="small"
+                    className="navds-pagination__prev-text"
+                >
+                    Forrige
+                </BodyShort>
+            </Item>
+            <BodyShort>{side}</BodyShort>
+            <Item
+                className={cl("navds-pagination__prev-next", "navds-pagination--prev-next--with-text", {
+                    "navds-pagination--invisible": antallTreffPåSide !== ANTALL_RESULTATER_PER_SIDE,
+                })}
+                disabled={antallTreffPåSide !== ANTALL_RESULTATER_PER_SIDE}
+                onClick={() => endreSide?.(side + 1)}
+                page={side + 1}
+                size="small"
+                icon={
+                    <Next
+                        className="navds-pagination__prev-next-icon"
+                        aria-hidden={true}
+                    />
+                }
+                iconPosition="right"
             >
-                Neste
-            </BodyShort>
-        </Item>
-        <Detail size={"small"}>
-            Tallene viser offisiell sykefraværsstatistikk for andre kvartal 2022.
-        </Detail>
-    </div>)
+                <BodyShort
+                    size="small"
+                    className="navds-pagination__next-text"
+                >
+                    Neste
+                </BodyShort>
+            </Item>
+            <Detail size="small">
+                Tallene viser offisiell sykefraværsstatistikk for andre kvartal 2022.
+            </Detail>
+        </Container>)
 }
