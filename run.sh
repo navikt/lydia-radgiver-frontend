91f8d8905ffe869c3d6b83368ccb29db97e912a7
Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
2022-10-24 11:27:34 +0200

    Flytter nedlasting av temporær database-import til /tmp
    I tilfellet oppstartsscriptet feiler slipper man å ha en database-dump liggende som man kan ved en feil fort kan commite til prosjektet.

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 
 docker-compose up -d
-curl -o db_script.sql https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia_api_container_db_localhost-2022_09_29_10_21_57-dump.sql
+DB_DUMP=/tmp/db_script.sql
+curl -o $DB_DUMP https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia_api_container_db_localhost-2022_09_29_10_21_57-dump.sql
 sleep 3
-PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f db_script.sql
-rm db_script.sql
+PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f $DB_DUMP
+rm $DB_DUMP
 cd client
 npm install
 npm run dev
