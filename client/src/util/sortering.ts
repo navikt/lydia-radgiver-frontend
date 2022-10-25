f67b04eb2610eb760b0bb375f9f46619e57334f5
Ingrid Fosså <fossa.ingrid@gmail.com>
2022-10-25 12:35:01 +0200

    Fiksa sorteringsrekkefølgje i dropdowns slik at den bruker rekkefølgja frå det norske alfabetet
    Co-authored-by: Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>

--- client/src/util/sortering.ts
+++ client/src/util/sortering.ts
@@ -1,7 +1,7 @@
 import {SykefraversstatistikkVirksomhet} from "../domenetyper";
 
 export const sorterAlfabetisk = (a: string, b: string) =>
-    a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase());
+    a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase(), "no");
 
 export const sorterStatistikkPåSisteÅrstallOgKvartal = (a: SykefraversstatistikkVirksomhet, b: SykefraversstatistikkVirksomhet) =>
     a.arstall !== b.arstall ? b.arstall - a.arstall : b.kvartal - a.kvartal
