bc40cc80c7075088aa5825be001139463591535a
Ingrid Fosså <fossa.ingrid@gmail.com>
2022-10-26 16:58:32 +0200

    Legg til enum for nav-fargar
    - gjer det lettare å bruke fargane frå nav sitt designsystem
    - meir robust for endring i fargane frå designsystemet enn hex-verdiar, sidan enumen refererer til css-variablane i designsystemet
    - tek mindre plass å referere til enum enn å bruke css-variablar

--- client/src/styling/farger.ts
+++ client/src/styling/farger.ts
@@ -9,3 +9,36 @@ export enum FiaFarger {
     grønn = "#99DEAD",      // green200
     gul = "#FFD799",        // orange200
 }
+
+// https://aksel.nav.no/designsystem/side/color
+export enum NavFarger {
+    white = "var(--navds-global-color-white)",
+
+    gray50 = "var(--navds-global-color-gray-50)",
+    gray100 = "var(--navds-global-color-gray-100)",
+    gray200 = "var(--navds-global-color-gray-200)",
+    gray300 = "var(--navds-global-color-gray-300)",
+    gray400 = "var(--navds-global-color-gray-400)",
+    gray500 = "var(--navds-global-color-gray-500)",
+    gray600 = "var(--navds-global-color-gray-600)",
+    gray700 = "var(--navds-global-color-gray-700)",
+    gray800 = "var(--navds-global-color-gray-800)",
+    gray900 = "var(--navds-global-color-gray-900)",
+
+    red50 = "var(--navds-global-color-red-50)",
+    red100 = "var(--navds-global-color-red-100)",
+    red200 = "var(--navds-global-color-red-200)",
+    red300 = "var(--navds-global-color-red-300)",
+    red400 = "var(--navds-global-color-red-400)",
+    red500 = "var(--navds-global-color-red-500)",
+    red600 = "var(--navds-global-color-red-600)",
+    red700 = "var(--navds-global-color-red-700)",
+    red800 = "var(--navds-global-color-red-800)",
+    red900 = "var(--navds-global-color-red-900)",
+
+    // Semantiske fargar
+    text = gray900,
+    textMuted = gray600,
+    textInverted = white,
+    feedbackDangerText = red500,
+}
