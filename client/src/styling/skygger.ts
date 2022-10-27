280961ccd8738f95299e3749d8efa25544f633db
Ingrid Fosså <fossa.ingrid@gmail.com>
2022-10-27 10:34:33 +0200

    Legg til enum for skuggar frå designsystemet slik at skugge-styling vert lettare å lese

---
+++ client/src/styling/skygger.ts
@@ -0,0 +1,9 @@
+export const enum Skygger {
+    xsmall = "var(--navds-shadow-xsmall)",
+    small = "var(--navds-shadow-small)",
+    medium = "var(--navds-shadow-medium)",
+    large = "var(--navds-shadow-large)",
+    xlarge = "var(--navds-shadow-xlarge)",
+    focus = "var(--navds-shadow-focus)",
+    focusInverted = "var(--navds-shadow-focus-inverted)",
+}
