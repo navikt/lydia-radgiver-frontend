cf03751f9d8d3ec0e4f4ffc49c9e475ef46e8346
Ulrik Antoniussen Halmøy <ulrik.antoniussen.halmoy@nav.no>
2022-11-07 12:29:46 +0100

    Fikser lint-feil i en story
    Co-authored-by: Ingrid Fosså <fossa.ingrid@gmail.com>

--- client/src/Pages/Prioritering/AntallArbeidsforholdVelger.stories.tsx
+++ client/src/Pages/Prioritering/AntallArbeidsforholdVelger.stories.tsx
@@ -6,10 +6,9 @@ export default {
     component: AntallArbeidsforholdVelger,
 } as ComponentMeta<typeof AntallArbeidsforholdVelger>
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function,@typescript-eslint/no-unused-vars
 export const Hovedstory = () => (
     <AntallArbeidsforholdVelger
         antallArbeidsforhold={{fra: NaN, til: NaN}}
-        endreAntallArbeidsforhold={(_) => {}}
+        endreAntallArbeidsforhold={() => { return }}
     />
 )
