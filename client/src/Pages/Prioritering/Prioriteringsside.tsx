b0e3f798a705f51c11b990dc19de77fb92cf6af0
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2022-11-29 12:14:15 +0100

    Fjern hilsen - Takk for avskjeden :)

--- client/src/Pages/Prioritering/Prioriteringsside.tsx
+++ client/src/Pages/Prioritering/Prioriteringsside.tsx
@@ -11,42 +11,12 @@ import { SykefraversstatistikkVirksomhet } from "../../domenetyper";
 import { statiskeSidetitler, TittelContext } from "./TittelContext";
 import { contentSpacing } from "../../styling/contentSpacing";
 import { useFiltervisningState } from "../Virksomhet/filtervisning-reducer";
-import {erIDev} from "../../components/Dekoratør/Dekoratør";
 
 const Container = styled.div`
     padding: ${contentSpacing.mobileY} 0;
 `;
 
 export const ANTALL_RESULTATER_PER_SIDE = 100;
-
-
-function erEtterJegStikkerBuhu(): boolean {
-    const jegStikkerBuhu = new Date(2022, 10, 25);
-    jegStikkerBuhu.setHours(0, 0, 0, 0);
-    return new Date() > jegStikkerBuhu;
-}
-
-
-function enHilsenFraUlrik() {
-    const alleStemmer = window.speechSynthesis.getVoices();
-    const voice = alleStemmer.find(voice => voice.lang == "nb-NO")
-        ?? alleStemmer.find(voice => voice.lang == "sv-SE")
-        ?? alleStemmer[0]
-    const minLilleBeskjed = new SpeechSynthesisUtterance(`
-        Hei mine kjære kollegaer. Nå er min tid forbi. 
-        Det har vært veldig hyggelig og lærerikt å jobbe med dere alle sammen.
-        Ta vare, og lykke til videre!
-        Vi sees før eller siden :-)
-        Hilsen Ulrik
-    `);
-    minLilleBeskjed.voice = voice
-    minLilleBeskjed.rate = 0.6
-    minLilleBeskjed.pitch = 0.1
-    minLilleBeskjed.onend = () => {
-        document.body.style.fontFamily = "Comic Sans MS";
-    }
-    window.speechSynthesis.speak(minLilleBeskjed)
-}
 
 const Prioriteringsside = () => {
     const { oppdaterTittel } = useContext(TittelContext);
@@ -104,9 +74,6 @@ const Prioriteringsside = () => {
                 filtervisning={filtervisning}
                 søkPåNytt={() => {
                     oppdaterSide(1);
-                    if(erIDev && erEtterJegStikkerBuhu()) {
-                        enHilsenFraUlrik()
-                    }
                 }}
             />
             <br />
