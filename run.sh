4a7ae35c5bd633e13e931d847583aa1d0e232f5b
Ingrid Fosså <fossa.ingrid@gmail.com>
2022-12-05 15:14:23 +0100

    Fiks lokalt docker-compose-oppsett

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 docker-compose up -d
 DB_DUMP=/tmp/db_script.sql
-curl -o $DB_DUMP https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia_api_container_db_localhost-2022_09_29_10_21_57-dump.sql
+curl -o $DB_DUMP https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia_api_container_db_localhost-2022_12_05_10_57_42-dump.sql
 sleep 3
-PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f $DB_DUMP
+PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f $DB_DUMP > /dev/null
 rm $DB_DUMP
 cd client
 npm install
