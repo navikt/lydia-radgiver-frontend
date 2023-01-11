c4ee744304652577171fc4028a9f274c94dc5bb6
Per-Christian Nielsen <per-christian.nielsen@nav.no>
2023-01-11 14:47:18 +0100

    Bruker ny databasedump.
    Co-authored-by: Christian Klem <christian.klem@nav.no>
    Co-authored-by: Thomas Dufourd <thomas.dufourd@gmail.com>
    Co-authored-by: Ingrid Fosså <ingrid.fossa@nav.no>

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 docker-compose up -d
 DB_DUMP=/tmp/db_script.sql
-curl -o $DB_DUMP https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia_api_container_db_localhost-2022_12_05_10_57_42-dump.sql
+curl -o $DB_DUMP https://raw.githubusercontent.com/navikt/lydia-api/main/scripts/db/lydia_api_container_db_localhost-2023_01_11_13_11_11-dump.sql
 sleep 3
 PGPASSWORD=test psql -h localhost -p 5432 -U postgres -f $DB_DUMP > /dev/null
 rm $DB_DUMP
