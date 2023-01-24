f424efbfc88c88d2dcb2458f19ea93b1876e5eae
nimajimale <nima0512@gmail.com>
2023-01-24 11:05:23 +0100

    Refaktorer: siste4kvartal
    Co-authored-by: Ingrid Fosså <ingrid.fossa@nav.no>
    Co-authored-by: Christian Klem <christian.klem@nav.no>

--- client/src/util/sortering.ts
+++ client/src/util/sortering.ts
@@ -1,10 +1,7 @@
-import {Kvartal, Virksomhetsdetaljer} from "../domenetyper";
+import { Kvartal } from "../domenetyper";
 
 export const sorterAlfabetisk = (a: string, b: string) =>
     a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase(), "no");
-
-export const sorterStatistikkPåSisteÅrstallOgKvartal = (a: Virksomhetsdetaljer, b: Virksomhetsdetaljer) =>
-    a.arstall !== b.arstall ? b.arstall - a.arstall : b.kvartal - a.kvartal
 
 export const sorterKvartalStigende = (a: Kvartal, b: Kvartal) =>
     a.årstall !== b.årstall ? a.årstall - b.årstall : a.kvartal - b.kvartal
