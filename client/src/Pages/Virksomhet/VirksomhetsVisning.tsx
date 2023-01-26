36950910c3cb0a610dce6cbd3d847fa6cf6748bf
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-01-26 14:44:09 +0100

    Trekk ut gjenbrukt styling til ein konstant slik at den kan forklarast gjennom namngiving

--- client/src/Pages/Virksomhet/VirksomhetsVisning.tsx
+++ client/src/Pages/Virksomhet/VirksomhetsVisning.tsx
@@ -1,64 +1,25 @@
 import styled from "styled-components";
+import { Tabs } from "@navikt/ds-react";
 import { IASak, Sakshistorikk, Virksomhet, } from "../../domenetyper";
 import { Samarbeidshistorikk } from "./Samarbeidshistorikk/Samarbeidshistorikk";
 import { Virksomhetsoversikt } from "./Virksomhetsoversikt/Virksomhetsoversikt";
-import { contentSpacing } from "../../styling/contentSpacing";
-import { Tabs } from "@navikt/ds-react";
+import { contentSpacing, strekkBakgrunnenHeltUtTilKantenAvSida } from "../../styling/contentSpacing";
 import { NavFarger } from "../../styling/farger";
-import { desktopAndUp, largeDesktopAndUp } from "../../styling/breakpoint";
 
 const Container = styled.div`
   padding-top: ${contentSpacing.mobileY};
   height: 100%;
 
   background-color: ${NavFarger.white};
-  
-  // Gjer at bakgrunnsgfargen dekkjer heile breidda av skjermen
-  margin-left: -${contentSpacing.mobileX};
-  margin-right: -${contentSpacing.mobileX};
-  padding-left: ${contentSpacing.mobileX};
-  padding-right: ${contentSpacing.mobileX};
-
-  ${desktopAndUp} {
-    margin-left: -${contentSpacing.desktopX};
-    margin-right: -${contentSpacing.desktopX};
-    padding-left: ${contentSpacing.desktopX};
-    padding-right: ${contentSpacing.desktopX};
-  }
-
-  ${largeDesktopAndUp} {
-    margin-left: -${contentSpacing.largeDesktopX};
-    margin-right: -${contentSpacing.largeDesktopX};
-    padding-left: ${contentSpacing.largeDesktopX};
-    padding-right: ${contentSpacing.largeDesktopX};
-  }
+  ${strekkBakgrunnenHeltUtTilKantenAvSida}
 `;
 
 const StyledPanel = styled(Tabs.Panel)`
   padding-top: 1.5rem;
   height: 100%;
-
-  background-color: ${NavFarger.gray100};
   
-  // Gjer at bakgrunnsgfargen dekkjer heile breidda av skjermen
-  margin-left: -${contentSpacing.mobileX};
-  margin-right: -${contentSpacing.mobileX};
-  padding-left: ${contentSpacing.mobileX};
-  padding-right: ${contentSpacing.mobileX};
-
-  ${desktopAndUp} {
-    margin-left: -${contentSpacing.desktopX};
-    margin-right: -${contentSpacing.desktopX};
-    padding-left: ${contentSpacing.desktopX};
-    padding-right: ${contentSpacing.desktopX};
-  }
-
-  ${largeDesktopAndUp} {
-    margin-left: -${contentSpacing.largeDesktopX};
-    margin-right: -${contentSpacing.largeDesktopX};
-    padding-left: ${contentSpacing.largeDesktopX};
-    padding-right: ${contentSpacing.largeDesktopX};
-  }
+  background-color: ${NavFarger.gray100};
+  ${strekkBakgrunnenHeltUtTilKantenAvSida}
 `;
 
 interface Props {
--- client/src/styling/contentSpacing.ts
+++ client/src/styling/contentSpacing.ts
@@ -1,6 +1,31 @@
+import { css } from "styled-components";
+import { desktopAndUp, largeDesktopAndUp } from "./breakpoint";
+
 export const contentSpacing =  {
     mobileX: '8px',
     mobileY: `${24/16}rem`,
     desktopX: '5rem',
     largeDesktopX: '10rem',
 }
+
+export const strekkBakgrunnenHeltUtTilKantenAvSida = css`
+  // Gjer at eit element dekkjer heile breidda av skjermen
+  margin-left: -${contentSpacing.mobileX};
+  margin-right: -${contentSpacing.mobileX};
+  padding-left: ${contentSpacing.mobileX};
+  padding-right: ${contentSpacing.mobileX};
+
+  ${desktopAndUp} {
+    margin-left: -${contentSpacing.desktopX};
+    margin-right: -${contentSpacing.desktopX};
+    padding-left: ${contentSpacing.desktopX};
+    padding-right: ${contentSpacing.desktopX};
+  }
+
+  ${largeDesktopAndUp} {
+    margin-left: -${contentSpacing.largeDesktopX};
+    margin-right: -${contentSpacing.largeDesktopX};
+    padding-left: ${contentSpacing.largeDesktopX};
+    padding-right: ${contentSpacing.largeDesktopX};
+  }
+`
