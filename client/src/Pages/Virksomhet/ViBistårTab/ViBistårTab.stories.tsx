364c5dc841423d042a528139cf8e55ac12874254
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-01-31 16:04:01 +0100

    Legg til story for Vi bistår-fane

---
+++ client/src/Pages/Virksomhet/ViBistårTab/ViBistårTab.stories.tsx
@@ -0,0 +1,11 @@
+import { ViBistårTab } from "./ViBistårTab";
+import { ComponentMeta } from "@storybook/react";
+
+export default {
+    title: "Virksomhet/Vi bistår/Vi bistår-tab",
+    component: ViBistårTab,
+} as ComponentMeta<typeof ViBistårTab>
+
+export const ViBistarTabStory = () => (
+    <ViBistårTab />
+)
