c43621790a990b4ca857edb098257454bcee4a56
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-02-03 12:32:44 +0100

    Lagar zod-typar for iaSakLeveranse m/familie
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

--- client/src/domenetyper/iaLeveranse.ts
+++ client/src/domenetyper/iaLeveranse.ts
@@ -1,14 +1,44 @@
-export interface IATjeneste {
-    id: number,
-    navn: string,
-}
+import { z } from "zod";
+import { datoSchema } from "./domenetyper";
 
-export interface IATjenesteModul {
-    id: number,
-    iaTjeneste: number,
-    navn: string,
-}
+const iaTjenesteSchema = z.object({
+    id: z.number(),
+    navn: z.string()
+})
 
+export type IATjeneste = z.infer<typeof iaTjenesteSchema>;
+
+const iaTjenesteModulSchema = z.object({
+    id: z.number(),
+    iaTjeneste: z.number(),
+    navn: z.string(),
+})
+
+export type IATjenesteModul = z.infer<typeof iaTjenesteModulSchema>;
+
+const IA_SAK_LEVERANSE_STATUSER = [
+    "UNDER_ARBEID",
+    "LEVERT",
+    // "AVBRUTT"?
+] as const;
+
+const IASakLeveranseStatusEnum = z.enum(IA_SAK_LEVERANSE_STATUSER);
+
+type IASakLeveranseStatus = z.infer<typeof IASakLeveranseStatusEnum>
+
+export const iaSakLeveranseSchema = z.object({
+    id: z.number(),
+    saksnummer: z.string(),
+    modul: z.object({
+        id: z.number(),
+        iaTjeneste: iaTjenesteSchema,
+        navn: z.string(),
+    }),
+    frist: datoSchema,
+    status: IASakLeveranseStatusEnum,
+})
+
+export type IASakLeveranse = z.infer<typeof iaSakLeveranseSchema>
 
 export interface NyIASakLeveranseDTO {
     saksnummer: string;
