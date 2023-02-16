35866949ca67a8458f9719757ac814d67d67d002
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-02-16 15:56:37 +0100

    Legg til story for leveranseoversikt

---
+++ client/src/Pages/Virksomhet/ViBistårTab/LeveranseOversikt.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentMeta } from "@storybook/react";
+import { iaSakViBistår } from "../mocks/iaSakMock";
+import { SimulerMobilWrapper, SimulerTabletWrapper } from "../../../../.storybook/SkjermstørrelseSimuleringer";
+import { LeveranseOversikt } from "./LeveranseOversikt";
+
+export default {
+    title: "Virksomhet/Vi bistår/Leveranseoversikt",
+    component: LeveranseOversikt,
+} as ComponentMeta<typeof LeveranseOversikt>
+
+export const Hovedstory = () => (
+    <LeveranseOversikt iaSak={iaSakViBistår} />
+)
+
+export const HovedstoryTablet = () => (
+    <SimulerTabletWrapper>
+        <LeveranseOversikt iaSak={iaSakViBistår} />
+    </SimulerTabletWrapper>
+)
+
+export const HovedstoryMobil = () => (
+    <SimulerMobilWrapper>
+        <LeveranseOversikt iaSak={iaSakViBistår} />
+    </SimulerMobilWrapper>
+)
