084015e076110a9312dffb004ce925e4326824a5
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-02-28 16:01:18 +0100

    Gje mock-brukarar ulike nav-identar

--- client/src/Pages/Prioritering/mocks/innloggetAnsattMock.ts
+++ client/src/Pages/Prioritering/mocks/innloggetAnsattMock.ts
@@ -5,15 +5,7 @@ const FEM_SEKUNDER_MS = 1000 * 5
 
 export const brukerMedGyldigToken : Brukerinformasjon = {
     navn: "Gyldig Bruker",
-    ident: "X12345",
-    epost: "a@b.com",
-    rolle: "Superbruker",
-    tokenUtløper: Date.now() + TRE_TIMER_MS
-}
-
-export const brukerMedVeldigLangtNavn : Brukerinformasjon = {
-    navn: "Albus Persifal Ulfrik Brian Humlesnurr",
-    ident: "X12345",
+    ident: "B12345",
     epost: "a@b.com",
     rolle: "Superbruker",
     tokenUtløper: Date.now() + TRE_TIMER_MS
@@ -21,8 +13,16 @@ export const brukerMedVeldigLangtNavn : Brukerinformasjon = {
 
 export const brukerMedTokenSomHolderPåÅLøpeUt : Brukerinformasjon = {
     navn: "Utgått Bruker",
-    ident: "X12345",
+    ident: "U12345",
     epost: "a@b.com",
     rolle: "Superbruker",
     tokenUtløper: Date.now() + FEM_SEKUNDER_MS
+}
+
+export const brukerMedVeldigLangtNavn : Brukerinformasjon = {
+    navn: "Albus Persifal Ulfrik Brian Humlesnurr",
+    ident: "H12345",
+    epost: "a@b.com",
+    rolle: "Superbruker",
+    tokenUtløper: Date.now() + TRE_TIMER_MS
 }
