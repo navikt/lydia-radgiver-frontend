c2fed78d5a8c7ef4ddb248b4aa8cad8e46ffb89b
Christian Klem <christian.klem@nav.no>
2023-03-02 09:25:10 +0100

    Oppdater krypteringsalgoritme til gcm
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- server/crypto.ts
+++ server/crypto.ts
@@ -1,18 +1,7 @@
 import { randomBytes, createHash, createCipheriv, createDecipheriv } from "crypto";
 
-const ALGORITHM = "aes-256-ctr"; // AES-256
+const ALGORITHM = "aes-256-gcm"; // AES-256
 const HASH_ALGORITHM = "sha256";
-
-export async function decrypt(encrypted: string) {
-    const sha256 = createHash(HASH_ALGORITHM);
-    sha256.update(process.env.OBO_TOKEN_ENC_KEY);
-    const input = Buffer.from(encrypted, "base64");
-    const iv = input.slice(0, 16);
-    const decipher = createDecipheriv(ALGORITHM, sha256.digest(), iv);
-    const cipherText = input.slice(16);
-    const decrypted = Buffer.concat([decipher.update(cipherText), decipher.final()]);
-    return decrypted.toString();
-}
 
 export async function encrypt(secret: string) {
     const sha256 = createHash(HASH_ALGORITHM);
@@ -20,6 +9,21 @@ export async function encrypt(secret: string) {
     const iv = randomBytes(16);
     const cipher = createCipheriv(ALGORITHM, sha256.digest(), iv);
     const cipherText = cipher.update(Buffer.from(secret));
-    const encrypted = Buffer.concat([iv, cipherText, cipher.final()]);
+    const enc = cipher.final()
+    const encrypted = Buffer.concat([iv, cipher.getAuthTag(), cipherText, enc]);
     return encrypted.toString("base64");
 }
+
+export async function decrypt(encrypted: string) {
+    const sha256 = createHash(HASH_ALGORITHM);
+    sha256.update(process.env.OBO_TOKEN_ENC_KEY);
+    const input = Buffer.from(encrypted, "base64");
+    const iv = input.slice(0, 16);
+    const authTag = input.slice(16, 32)
+    const decipher = createDecipheriv(ALGORITHM, sha256.digest(), iv);
+    decipher.setAuthTag(authTag)
+    const cipherText = input.slice(32);
+    const decrypted = Buffer.concat([decipher.update(cipherText), decipher.final()]);
+    return decrypted.toString();
+}
+
