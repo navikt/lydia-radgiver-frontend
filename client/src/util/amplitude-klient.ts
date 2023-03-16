27c29633cd137491a8d2a4e930637fde567247a7
nimajimale <nima0512@gmail.com>
2023-03-16 10:14:43 +0100

    Legg til Amplitude Analytics
    Co-authored-by: Ingrid Fosså <ingrid.fossa@nav.no>
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>

---
+++ client/src/util/amplitude-klient.ts
@@ -0,0 +1,23 @@
+import amplitude, { AmplitudeClient } from "amplitude-js";
+
+const amplitudeKlient : AmplitudeClient = amplitude.getInstance();
+
+let initialized = false;
+
+const apiKeys = {
+    fiaProd: "747d79b00c945cf3e549ae0b197293bf",
+    fiaDev: "747f1d150abf4cad4248ff1d3f93e999"
+};
+export const loggSideLastet = (side: string) => {
+    if (!initialized) {
+        const apiKey = process.env.NODE_ENV === "production"
+            ? apiKeys.fiaProd
+            : apiKeys.fiaDev;
+
+        amplitudeKlient.init(apiKey, "",{
+            apiEndpoint: "amplitude.nav.no/collect"
+        });
+        initialized = true;
+    }
+    amplitudeKlient.logEvent("side-lastet",{side : side} );
+};
