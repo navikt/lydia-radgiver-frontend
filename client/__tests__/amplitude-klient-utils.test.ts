da17f8ae5a1da0e14a14318319adc3f051e9749d
Thomas Dufourd <thomas.dufourd@gmail.com>
2023-03-20 16:10:56 +0100

    Typo orgn -> orgnr
    Co-authored-by: Christian Klem <86653001+klechr@users.noreply.github.com>

--- client/__tests__/amplitude-klient-utils.test.ts
+++ client/__tests__/amplitude-klient-utils.test.ts
@@ -1,6 +1,6 @@
 import { maskerOrgnr } from "../src/util/amplitude-klient-utils";
 
-describe("Masker orgn (9 siffer) i url", () => {
+describe("Masker orgnr (9 siffer) i url", () => {
     test("URL blir uendreet dersom det ikke er noe å maskere", () => {
         expect(maskerOrgnr(undefined)).toBe('');
         expect(maskerOrgnr('')).toBe('');
