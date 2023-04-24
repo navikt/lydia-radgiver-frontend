1b06fe56b8ae31f3934924e4da779baf6651f62e
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-04-24 16:43:45 +0200

    Bytt til default-export av storybook-instillinger etter oppdatering til v7.0

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,23 +1,27 @@
-module.exports = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-mdx-gfm"],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {}
-  },
-  staticDirs: ["../public"],
-  async viteFinal(config, {
-    configType
-  }) {
-    if (configType === "PRODUCTION") {
-      return {
-        ...config,
-        base: "/lydia-radgiver-frontend/"
-      };
+import { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+    stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-mdx-gfm"],
+    framework: {
+        name: "@storybook/react-vite",
+        options: {}
+    },
+    staticDirs: ["../public"],
+    async viteFinal(config, {
+        configType
+    }) {
+        if (configType === "PRODUCTION") {
+            return {
+                ...config,
+                base: "/lydia-radgiver-frontend/"
+            };
+        }
+        return config;
+    },
+    docs: {
+        autodocs: true
     }
-    return config;
-  },
-  docs: {
-    autodocs: true
-  }
 };
+
+export default config;
--- client/.storybook/preview.ts
+++ client/.storybook/preview.ts
@@ -1,6 +1,7 @@
+import { Preview } from "@storybook/react";
+import { rest } from "msw";
 import { initialize, mswDecorator } from "msw-storybook-addon";
 import { NavFarger } from "../src/styling/farger";
-import { rest } from "msw";
 import { brukerMedVeldigLangtNavn } from "../src/Pages/Prioritering/mocks/innloggetAnsattMock";
 import {
     gjeldendePeriodePath,
@@ -17,85 +18,87 @@ import {
 import { samarbeidshistorikkMock } from "../src/Pages/Virksomhet/mocks/iaSakHistorikkMock";
 import { leveranserPerIATjeneste } from "../src/Pages/Virksomhet/mocks/leveranseMock";
 
-export const parameters = {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    backgrounds: {
-        default: 'canvasBackground',
-        values: [
-            {
-                name: 'canvasBackground',
-                value: NavFarger.canvasBackground,
-            },
-            {
-                name: 'white',
-                value: NavFarger.white,
-            },
-            {
-                name: 'limegreen-500',
-                value: '#A2AD00',
-            },
-            {
-                name: 'limegreen-50',
-                value: '#FDFFE6',
-            },
-            {
-                name: 'light',
-                value: '#F8F8F8',
-            },
-            {
-                name: 'dark',
-                value: '#333333',
-            },
-        ]
-    },
-    controls: {
-        matchers: {
-            color: /(background|color)$/i,
-            date: /Date$/,
+const preview: Preview = {
+    parameters: {
+        actions: {argTypesRegex: "^on[A-Z].*"},
+        backgrounds: {
+            default: 'canvasBackground',
+            values: [
+                {
+                    name: 'canvasBackground',
+                    value: NavFarger.canvasBackground,
+                },
+                {
+                    name: 'white',
+                    value: NavFarger.white,
+                },
+                {
+                    name: 'limegreen-500',
+                    value: '#A2AD00',
+                },
+                {
+                    name: 'limegreen-50',
+                    value: '#FDFFE6',
+                },
+                {
+                    name: 'light',
+                    value: '#F8F8F8',
+                },
+                {
+                    name: 'dark',
+                    value: '#333333',
+                },
+            ]
         },
-    },
-    msw: { // Handlers to be applied in every story, see https://github.com/mswjs/msw-storybook-addon/tree/d64eb49bf6bb190d25d7c86a2751386068dbda54#advanced-usage
-        handlers: {
-            innloggetAnsatt: [
-                rest.get('/innloggetAnsatt', (req, res, ctx) => {
-                    return res(ctx.json(brukerMedVeldigLangtNavn));
-                }),
-            ],
-            sykefraværsstatistikkSisteKvartal: [
-                rest.get(`${sykefraværsstatistikkPath}/:orgnummer/sistetilgjengeligekvartal`, (req, res, ctx) => {
-                    return res(
-                        ctx.json(sykefraværsstatistikkSisteKvartalMock[0])
-                    );
-                }),
-            ],
-            sykefraværsstatistikkSiste4Kvartal: [
-                rest.get(`${sykefraværsstatistikkPath}/:orgnummer/${siste4kvartalerPath}`, (req, res, ctx) => {
-                    return res(
-                        ctx.json(virksomhetsstatistikkSiste4KvartalerMock[0])
-                    );
-                }),
-            ],
-            gjeldendePeriodeSiste4Kvartal: [
-                rest.get(`${sykefraværsstatistikkPath}/${gjeldendePeriodePath}`, (req, res, ctx) => {
-                    return res(
-                        ctx.json(gjeldendePeriodeSiste4Kvartal)
-                    );
-                }),
-            ],
-            samarbeidshistorikk: [
-                rest.get(`${iaSakHistorikkPath}/:orgnummer`, (req, res, ctx) => {
-                    return res(
-                        ctx.json(samarbeidshistorikkMock)
-                    );
-                }),
-            ],
-            leveranser: [
-                rest.get(`${leveransePath}/:orgnummer/:saksnummer`, (req, res, ctx) => {
-                    return res(
-                        ctx.json(leveranserPerIATjeneste)
-                    );
-                }),
-            ],
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
+        },
+        msw: { // Handlers to be applied in every story, see https://github.com/mswjs/msw-storybook-addon/tree/d64eb49bf6bb190d25d7c86a2751386068dbda54#advanced-usage
+            handlers: {
+                innloggetAnsatt: [
+                    rest.get('/innloggetAnsatt', (req, res, ctx) => {
+                        return res(ctx.json(brukerMedVeldigLangtNavn));
+                    }),
+                ],
+                sykefraværsstatistikkSisteKvartal: [
+                    rest.get(`${sykefraværsstatistikkPath}/:orgnummer/sistetilgjengeligekvartal`, (req, res, ctx) => {
+                        return res(
+                            ctx.json(sykefraværsstatistikkSisteKvartalMock[0])
+                        );
+                    }),
+                ],
+                sykefraværsstatistikkSiste4Kvartal: [
+                    rest.get(`${sykefraværsstatistikkPath}/:orgnummer/${siste4kvartalerPath}`, (req, res, ctx) => {
+                        return res(
+                            ctx.json(virksomhetsstatistikkSiste4KvartalerMock[0])
+                        );
+                    }),
+                ],
+                gjeldendePeriodeSiste4Kvartal: [
+                    rest.get(`${sykefraværsstatistikkPath}/${gjeldendePeriodePath}`, (req, res, ctx) => {
+                        return res(
+                            ctx.json(gjeldendePeriodeSiste4Kvartal)
+                        );
+                    }),
+                ],
+                samarbeidshistorikk: [
+                    rest.get(`${iaSakHistorikkPath}/:orgnummer`, (req, res, ctx) => {
+                        return res(
+                            ctx.json(samarbeidshistorikkMock)
+                        );
+                    }),
+                ],
+                leveranser: [
+                    rest.get(`${leveransePath}/:orgnummer/:saksnummer`, (req, res, ctx) => {
+                        return res(
+                            ctx.json(leveranserPerIATjeneste)
+                        );
+                    }),
+                ],
+            }
         }
     }
 };
@@ -112,3 +115,5 @@ initialize({
 
 // Provide the MSW addon decorator globally
 export const decorators = [mswDecorator];
+
+export default preview;
