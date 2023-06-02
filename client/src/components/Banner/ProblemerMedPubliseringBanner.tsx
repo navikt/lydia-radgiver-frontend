baac89f7cda22b451c57d3801f9a756452a3bc7a
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-06-02 09:59:05 +0200

    Oppdater tekst i banner slik at den får plass på éi linje
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

--- client/src/components/Banner/ProblemerMedPubliseringBanner.tsx
+++ client/src/components/Banner/ProblemerMedPubliseringBanner.tsx
@@ -2,19 +2,15 @@ import styled from "styled-components";
 import { BodyShort, Heading } from "@navikt/ds-react";
 import { BannerMedLukkeknapp } from "./BannerMedLukkeknapp";
 
-const StyledBodyShort = styled(BodyShort)`
-  width: 70em;
-`;
-
 export const ProblemerMedPubliseringBanner = () => {
     return (
         <BannerMedLukkeknapp variant="info">
             <Heading size="xsmall">
-                Tekniske problemer med oppdatering av sykefraværsstatistikk for 1. kvartal 2023
+                Tekniske problemer med oppdatering av sykefraværs&shy;statistikk for 1. kvartal 2023
             </Heading>
-            <StyledBodyShort>
-                Vi jobber med å løse problemet og kommer tilbake med mer informasjon når problemet er løst.
-            </StyledBodyShort>
+            <BodyShort>
+                Vi jobber med å løse problemet og kommer tilbake med mer informasjon når vi har løst det.
+            </BodyShort>
         </BannerMedLukkeknapp>
     )
 }
