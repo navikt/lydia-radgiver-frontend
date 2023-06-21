19d3ef7f5bc94bfef7edf27edad830eeac8b477c
Christian Klem <christian.klem@nav.no>
2023-06-21 09:43:28 +0200

    Oppdater storybook til 7.0.22 - samt fjern (forhåpentligvis) unødvendige addons
    (unnskyld om jeg ødela noe ingrid :D )

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -2,7 +2,7 @@ import { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
     stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-mdx-gfm", '@storybook/addon-styling'],
+    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
     framework: {
         name: "@storybook/react-vite",
         options: {}
