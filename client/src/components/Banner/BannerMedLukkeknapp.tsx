1cbc093de067f725716743bcdc757415a7c5fcb6
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-07-04 17:33:43 +0200

    Tek i bruk prop for closeButton frå designsystemet
    Denne var ny i @navikt/ds-react 4.5.0.

--- client/src/components/Banner/BannerMedLukkeknapp.tsx
+++ client/src/components/Banner/BannerMedLukkeknapp.tsx
@@ -1,38 +1,13 @@
-import { ReactNode, useState } from "react";
-import styled from "styled-components";
-import { AlertProps, Button } from "@navikt/ds-react";
+import { useState } from "react";
+import { AlertProps } from "@navikt/ds-react";
 import { Banner } from "./Banner";
-import { NavFarger } from "../../styling/farger";
 
-export const StyledBanner = styled(Banner)`
-  position: relative; // For å halde lukk-knappen (position: absolute) inne i banneret
-  padding-right: 5rem; // Hindrar overlapp mellom lukk-knapp og tekst
-`;
-
-const Lukkeknapp = styled(Button).attrs({ size: "small", variant: "secondary" })`
-  position: absolute;
-  right: 0.5rem;
-  bottom: 0.5rem;
-  background: ${NavFarger.white}; // "secondary"-knapp frå DS har per 2023-02-27 ingen bakgrunn. Dette gjer at vi får for låg kontrast på "Error"-alert.
-`;
-
-interface Props extends AlertProps {
-    children: ReactNode;
-}
-
-export const BannerMedLukkeknapp = ({ children, ...props }: Props) => {
+export const BannerMedLukkeknapp = ({ children, ...props }: AlertProps) => {
     const [visBanner, setVisBanner] = useState(true);
 
-    if (!visBanner) return null;
-
-    const lukkMeg = () => {
-        setVisBanner(false)
-    }
-
     return (
-        <StyledBanner {...props}>
+        visBanner && <Banner {...props} closeButton onClose={() => setVisBanner(false)}>
             {children}
-            <Lukkeknapp onClick={lukkMeg} size="small" variant="secondary">Lukk</Lukkeknapp>
-        </StyledBanner>
+        </Banner>
     )
 }
