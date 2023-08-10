7cf8a26a3b787361bc2064cd6251be0b3017e50a
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-08-10 14:55:46 +0200

    Viser ikon og tekst på same linje når det er lite plass

--- client/src/components/EksternLenke.tsx
+++ client/src/components/EksternLenke.tsx
@@ -1,12 +1,10 @@
-import {Link, LinkProps} from "@navikt/ds-react";
-import {ExternalLinkIcon} from "@navikt/aksel-icons";
+import { Link, LinkProps } from "@navikt/ds-react";
+import { ExternalLinkIcon } from "@navikt/aksel-icons";
 
 export const EksternLenke = ({children, target, ...rest}: LinkProps) =>
-    <Link
-        target={target ?? "_blank"}
-        {...rest}>
+    <Link target={target ?? "_blank"} {...rest}>
         <span>
             {children}
-            <ExternalLinkIcon style={{paddingLeft: "0.3rem", paddingTop: "0.3rem"}} title="(åpner i en ny fane)"/>
         </span>
+        <ExternalLinkIcon fontSize="0.9em" title="(åpner i en ny fane)" />
     </Link>
