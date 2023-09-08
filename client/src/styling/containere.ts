7fae1a208931a7b58ea2f45ddab59e8fdf42ffc8
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-09-08 14:19:35 +0200

    Make Samarbeidshistorikk great again
    aka
    
    fiksar styling av element i samarbeidshistorikk
    
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>

--- client/src/styling/containere.ts
+++ client/src/styling/containere.ts
@@ -11,7 +11,7 @@ export const hvitBoksMedSkygge = css`
 `
 
 export const tabInnholdStyling = css`
-  padding: 0.75rem 1.5rem 1.5rem;
+  padding: 1.5rem;
   background: ${NavFarger.white};
   border-radius: ${BorderRadius.medium};
   box-shadow: ${Skygger.small};
