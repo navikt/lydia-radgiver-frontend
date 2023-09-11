62ddbc197b8f4b3337e96254ea325fab0b7fda2a
Thomas Dufourd <thomas.dufourd@gmail.com>
2023-09-11 12:42:53 +0200

    Endrer 'snitt' --> 'gjennomsnitt'

--- client/src/Pages/Prioritering/Filter/BransjeEllerNæringDropdown.tsx
+++ client/src/Pages/Prioritering/Filter/BransjeEllerNæringDropdown.tsx
@@ -9,8 +9,8 @@ export const BransjeEllerNæringDropdown = ({valgtSnittfilter, endreSnittfilter}
     return (
         <Select label="Bransje/næring" value={valgtSnittfilter} onChange={e => endreSnittfilter(e.target.value)}>
             <option key={"empty-status"} value={""}>Alle</option>
-            <option key={"OVER"} value={"BRANSJE_NÆRING_OVER"}>Sykefravær over snittet</option>
-            <option key={"UNDER_ELLER_LIK"} value={"BRANSJE_NÆRING_UNDER_ELLER_LIK"}>Sykefravær under eller lik snittet</option>
+            <option key={"OVER"} value={"BRANSJE_NÆRING_OVER"}>Sykefravær over gjennomsnitt</option>
+            <option key={"UNDER_ELLER_LIK"} value={"BRANSJE_NÆRING_UNDER_ELLER_LIK"}>Sykefravær under eller lik gjennomsnitt</option>
         </Select>
     )
 }
