d547fc8238f25b42119c8ede24584c66a1c91b5f
Thomas Dufourd <thomas.dufourd@gmail.com>
2023-09-12 13:14:44 +0200

    Forbedring av tekst: bruk av bestemt form i snittfilter dropdown

--- client/src/Pages/Prioritering/Filter/BransjeEllerNæringDropdown.tsx
+++ client/src/Pages/Prioritering/Filter/BransjeEllerNæringDropdown.tsx
@@ -9,8 +9,8 @@ export const BransjeEllerNæringDropdown = ({valgtSnittfilter, endreSnittfilter}
     return (
         <Select label="Sykefravær sammenlignet med bransje/næring" value={valgtSnittfilter} onChange={e => endreSnittfilter(e.target.value)}>
             <option key={"empty-status"} value={""}>Alle</option>
-            <option key={"OVER"} value={"BRANSJE_NÆRING_OVER"}>Over gjennomsnitt</option>
-            <option key={"UNDER_ELLER_LIK"} value={"BRANSJE_NÆRING_UNDER_ELLER_LIK"}>Under eller lik gjennomsnitt</option>
+            <option key={"OVER"} value={"BRANSJE_NÆRING_OVER"}>Over gjennomsnittet</option>
+            <option key={"UNDER_ELLER_LIK"} value={"BRANSJE_NÆRING_UNDER_ELLER_LIK"}>Under eller lik gjennomsnittet</option>
         </Select>
     )
 }
