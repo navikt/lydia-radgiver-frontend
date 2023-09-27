338470ce9240fdd55e3b30ab8f3a2319f7e90ed6
nimajimale <nima0512@gmail.com>
2023-09-27 15:48:57 +0200

    Legg til beskrivelse av graf Forbedre design av graf
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>

--- client/src/Pages/Virksomhet/Historiskstatistikk/Historiskstatistikk.tsx
+++ client/src/Pages/Virksomhet/Historiskstatistikk/Historiskstatistikk.tsx
@@ -1,43 +1,72 @@
-import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { CartesianGrid, Line, LineChart, ResponsiveContainer, Symbols, Tooltip, XAxis, YAxis } from "recharts";
 import React from "react";
 import { HistoriskStatistikk } from "../../../domenetyper/historiskstatistikk";
+import { BodyShort, Heading } from "@navikt/ds-react";
+import styled from "styled-components";
+import { tabInnholdStyling } from "../../../styling/containere";
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+
+  ${tabInnholdStyling};
+`;
 
 interface Props {
     historiskStatistikk: HistoriskStatistikk
 }
 
+const linjebredde = 2;
+const dotStrl = 40;
+
 export const Historiskstatistikk = ({ historiskStatistikk }: Props) => {
 
     const detSomSkalVises = historiskStatistikk.virksomhetsstatistikk.statistikk
-        .sort( (s1, s2)  => {
-            if((s1.årstall - s2.årstall) === 0) {
+        .sort((s1, s2) => {
+            if ((s1.årstall - s2.årstall) === 0) {
                 return s1.kvartal - s2.kvartal
             } else {
                 return s1.årstall - s2.årstall
             }
-        } )
+        })
         .map(
-        statistikk => {
-            return {
-                name: `${statistikk.årstall}-${statistikk.kvartal}`,
-                value: statistikk.sykefraværsprosent
+            statistikk => {
+                return {
+                    name: `${statistikk.årstall}-${statistikk.kvartal}`,
+                    value: statistikk.sykefraværsprosent
+                }
             }
-        }
-    )
-
+        )
 
     return (
-        <ResponsiveContainer minHeight={400}>
-            <LineChart
-                data={detSomSkalVises}
-                role="img"
-            >
-                <CartesianGrid strokeDasharray="3 3" stroke="#C6C2BF"/>
-                <Line type="monotone" dataKey="value" stroke="#000000"/>
-                <XAxis dataKey="name"/>
-                <YAxis/>
-                <Tooltip/>
-            </LineChart>
-        </ResponsiveContainer>
+        <Container>
+            <div>
+                <Heading spacing={true} size="large">Historisk statistikk</Heading>
+                <BodyShort>
+                    Her kan du se hvordan det legemeldte sykefraværet utvikler seg over tid.
+                </BodyShort>
+            </div>
+
+            <ResponsiveContainer minHeight={400}>
+                <LineChart
+                    data={detSomSkalVises}
+                    role="img"
+                >
+                    <CartesianGrid strokeDasharray="3 3" stroke="#C6C2BF" />
+                    <Line type="monotone"
+                          dataKey="value"
+                          stroke="red"
+                          strokeWidth={linjebredde}
+                          isAnimationActive={false}
+                          dot={<Symbols type={"circle"} size={dotStrl} fill={"red"} />}
+                    />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                </LineChart>
+            </ResponsiveContainer>
+        </Container>
     )
 }
