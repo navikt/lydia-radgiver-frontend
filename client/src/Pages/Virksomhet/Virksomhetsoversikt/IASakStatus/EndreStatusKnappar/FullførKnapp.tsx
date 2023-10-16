8e8a1f85f301ac238979a01c89c667f4728148a4
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-10-16 15:01:06 +0200

    Gjer leveranser obligatorisk ved fullføring av sak (i dev)
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

--- client/src/Pages/Virksomhet/Virksomhetsoversikt/IASakStatus/EndreStatusKnappar/FullførKnapp.tsx
+++ client/src/Pages/Virksomhet/Virksomhetsoversikt/IASakStatus/EndreStatusKnappar/FullførKnapp.tsx
@@ -4,18 +4,32 @@ import { HendelseMåBekreftesKnapp } from "./HendelseMåBekreftesKnapp";
 import { IASakshendelseKnapp } from "./IASakshendelseKnapp";
 import { useHentLeveranser } from "../../../../../api/lydia-api";
 import { FullførLeveranserFørstModal } from "../FullførLeveranserFørstModal";
+import { LeggTilLeveranserFørstModal } from "../LeggTilLeveranserFørstModal";
+import { erIDev } from "../../../../../components/Dekoratør/Dekoratør";
 
 interface Props {
     hendelse: GyldigNesteHendelse;
     sak: IASak;
 }
 
-export const FullførKnapp = ({hendelse, sak}: Props) => {
-    const {data: leveranserPåSak} = useHentLeveranser(sak.orgnr, sak.saksnummer);
+export const FullførKnapp = ({ hendelse, sak }: Props) => {
+    const { data: leveranserPåSak } = useHentLeveranser(sak.orgnr, sak.saksnummer);
+    const ikkeHarLeveranser = !leveranserPåSak?.length;
     const harLeveranserSomErUnderArbeid = leveranserPåSak?.flatMap((iaTjeneste) => iaTjeneste.leveranser)
         .some((leveranse) => leveranse.status === "UNDER_ARBEID")
     const [visModal, setVisModal] = useState(false);
 
+    if (erIDev && ikkeHarLeveranser) {
+        return (
+            <>
+                <IASakshendelseKnapp
+                    hendelsesType={hendelse.saksHendelsestype}
+                    onClick={() => setVisModal(true)}
+                />
+                <LeggTilLeveranserFørstModal visModal={visModal} lukkModal={() => setVisModal(false)} />
+            </>
+        )
+    }
     if (harLeveranserSomErUnderArbeid) {
         return (
             <>
---
+++ client/src/Pages/Virksomhet/Virksomhetsoversikt/IASakStatus/LeggTilLeveranserFørstModal.tsx
@@ -0,0 +1,29 @@
+import { BodyLong, Button, Modal } from "@navikt/ds-react";
+import { ModalKnapper } from "../../../../components/Modal/ModalKnapper";
+import { StyledModal } from "../../../../components/Modal/StyledModal";
+
+interface Props {
+    visModal: boolean;
+    lukkModal: () => void;
+}
+
+export const LeggTilLeveranserFørstModal = ({ visModal, lukkModal }: Props) => {
+    return (
+        <StyledModal
+            open={visModal}
+            onClose={lukkModal}
+            header={{ heading: "Saken har ingen IA-tjenester" }}>
+            <Modal.Body>
+                <BodyLong>
+                    For å gå videre må du registrere og fullføre minst én IA-tjeneste på saken.
+                </BodyLong>
+                <br />
+                <ModalKnapper>
+                    <Button variant="secondary" onClick={lukkModal}>
+                        Den er grei
+                    </Button>
+                </ModalKnapper>
+            </Modal.Body>
+        </StyledModal>
+    )
+}
