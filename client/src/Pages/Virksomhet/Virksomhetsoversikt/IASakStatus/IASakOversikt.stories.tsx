3a0169bd544d8fd3bc1c55b8ec609291e9dad25c
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-10-20 12:48:29 +0200

    La inn default msw handlers i Virksomhetsoversikt/IA-sak... i Storybook.

--- client/src/Pages/Virksomhet/Virksomhetsoversikt/IASakStatus/IASakOversikt.stories.tsx
+++ client/src/Pages/Virksomhet/Virksomhetsoversikt/IASakStatus/IASakOversikt.stories.tsx
@@ -11,15 +11,19 @@ import {
     iaSakVurderesMedEier,
     iaSakVurderesUtenEier,
 } from "../../mocks/iaSakMock";
+import { mswHandlers } from "../../../../../.storybook/mswHandlers";
+import { rest } from "msw";
+import { iaSakPath, innloggetAnsattPath } from "../../../../api/lydia-api";
+import { brukerSomErSaksbehandler } from "../../../Prioritering/mocks/innloggetAnsattMock";
 
 const meta = {
     title: "Virksomhet/Virksomhetsoversikt/IA-sak-oversikt (statusfelt)",
     component: IASakOversikt,
     parameters: {
         backgrounds: {
-            default: 'white'
-        }
-    }
+            default: "white",
+        },
+    },
 } satisfies Meta<typeof IASakOversikt>;
 
 export default meta;
@@ -29,6 +33,17 @@ export const IkkeAktiv: Story = {
     args: {
         orgnummer: "987654321",
     },
+    parameters: {
+        msw: [
+            ...mswHandlers,
+            rest.get(`${iaSakPath}/:orgnummer/aktiv`, (req, res, ctx) => {
+                return res(ctx.json(iaSakViBistår));
+            }),
+            rest.get(innloggetAnsattPath, (req, res, ctx) => {
+                return res(ctx.json(brukerSomErSaksbehandler));
+            }),
+        ],
+    },
 };
 
 export const VurderesUtenEier: Story = {
@@ -36,61 +51,138 @@ export const VurderesUtenEier: Story = {
         ...IkkeAktiv.args,
         iaSak: iaSakVurderesUtenEier,
     },
+    parameters: {
+        msw: [
+            ...mswHandlers,
+            rest.get(`${iaSakPath}/:orgnummer/aktiv`, (req, res, ctx) => {
+                return res(ctx.json(iaSakVurderesUtenEier));
+            }),
+            rest.get(innloggetAnsattPath, (req, res, ctx) => {
+                return res(ctx.json(brukerSomErSaksbehandler));
+            }),
+        ],
+    },
 };
 
 export const VurderesMedEierEier = {
     args: {
         ...IkkeAktiv.args,
         iaSak: iaSakVurderesMedEier,
-    }
+    },
+    parameters: {
+        msw: [
+            ...mswHandlers,
+            rest.get(`${iaSakPath}/:orgnummer/aktiv`, (req, res, ctx) => {
+                return res(ctx.json(iaSakVurderesMedEier));
+            }),
+        ],
+    },
 };
 
 export const Kontaktes = {
     args: {
         ...IkkeAktiv.args,
         iaSak: iaSakKontaktes,
-    }
+    },
+    parameters: {
+        msw: [
+            ...mswHandlers,
+            rest.get(`${iaSakPath}/:orgnummer/aktiv`, (req, res, ctx) => {
+                return res(ctx.json(iaSakKontaktes));
+            }),
+        ],
+    },
 };
-
 
 export const IkkeAktuell = {
     args: {
         ...IkkeAktiv.args,
         iaSak: iaSakIkkeAktuell,
-    }
+    },
+    parameters: {
+        msw: [
+            ...mswHandlers,
+            rest.get(`${iaSakPath}/:orgnummer/aktiv`, (req, res, ctx) => {
+                return res(ctx.json(iaSakIkkeAktuell));
+            }),
+        ],
+    },
 };
 
 export const Kartlegges = {
     args: {
         ...IkkeAktiv.args,
         iaSak: iaSakKartlegges,
-    }
+    },
+    parameters: {
+        msw: [
+            ...mswHandlers,
+            rest.get(`${iaSakPath}/:orgnummer/aktiv`, (req, res, ctx) => {
+                return res(ctx.json(iaSakKartlegges));
+            }),
+        ],
+    },
 };
 
 export const ViBistar = {
     args: {
         ...IkkeAktiv.args,
         iaSak: iaSakViBistår,
-    }
+    },
+    parameters: {
+        msw: [
+            ...mswHandlers,
+            rest.get(`${iaSakPath}/:orgnummer/aktiv`, (req, res, ctx) => {
+                return res(ctx.json(iaSakViBistår));
+            }),
+        ],
+    },
 };
 
 export const Fullfort = {
     args: {
         ...IkkeAktiv.args,
         iaSak: iaSakFullført,
-    }
+    },
+    parameters: {
+        msw: [
+            ...mswHandlers,
+            rest.get(`${iaSakPath}/:orgnummer/aktiv`, (req, res, ctx) => {
+                return res(ctx.json(iaSakFullført));
+            }),
+        ],
+    },
 };
 
 export const FullfortOgLukket = {
     args: {
         ...IkkeAktiv.args,
         iaSak: iaSakFullførtOgLukket,
-    }
+    },
+    parameters: {
+        msw: [
+            ...mswHandlers,
+            rest.get(`${iaSakPath}/:orgnummer/aktiv`, (req, res, ctx) => {
+                return res(ctx.json(iaSakFullførtOgLukket));
+            }),
+            rest.get(innloggetAnsattPath, (req, res, ctx) => {
+                return res(ctx.json(brukerSomErSaksbehandler));
+            }),
+        ],
+    },
 };
 
-export const IngenAktivitetPaLenge: Story = {
+export const IngenAktivitetPaLenge = {
     args: {
         ...IkkeAktiv.args,
         iaSak: iaSakIngenAktivitetPåOverEtKvartal,
-    }
-}
+    },
+    parameters: {
+        msw: [
+            ...mswHandlers,
+            rest.get(`${iaSakPath}/:orgnummer/aktiv`, (req, res, ctx) => {
+                return res(ctx.json(iaSakIngenAktivitetPåOverEtKvartal));
+            }),
+        ],
+    },
+};
