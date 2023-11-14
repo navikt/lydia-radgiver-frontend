2a5a0bc8e711eab15b1cef6ea248cb1c23b24f22
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-11-14 10:23:19 +0100

    Tek i mot informasjon om deaktiverte modular og IA-tjenester frå backend
    og deaktiverar gamle modular i mock-data
    
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

--- client/src/Pages/Virksomhet/mocks/leveranseMock.ts
+++ client/src/Pages/Virksomhet/mocks/leveranseMock.ts
@@ -12,12 +12,15 @@ export const iaTjenester: IATjeneste[] = [
     {
         id: 1,
         navn: "Redusere sykefravær",
+        deaktivert: false,
     }, {
         id: 2,
         navn: "Forebyggende arbeidsmiljøarbeid",
+        deaktivert: false,
     }, {
         id: 3,
         navn: "HelseIArbeid",
+        deaktivert: false,
     }
 ]
 
@@ -27,65 +30,96 @@ export const moduler: Modul[] = [
         id: 1,
         iaTjeneste: 1,
         navn: "Videreutvikle sykefraværsrutiner",
+        deaktivert: true,
     }, {
         id: 2,
         iaTjeneste: 1,
         navn: "Oppfølgingssamtalen",
+        deaktivert: true,
     }, {
         id: 3,
         iaTjeneste: 1,
         navn: "Tilretteleggingsplikt og medvirkningsplikt",
+        deaktivert: true,
     }, {
         id: 4,
         iaTjeneste: 1,
         navn: "Langvarige og/eller hyppig gjentakende sykefravær",
+        deaktivert: true,
+    }, {
+        id: 15,
+        iaTjeneste: 1,
+        navn: "Redusere sykefravær",
+        deaktivert: false,
     },
     // 2 - "Forebyggende arbeidsmiljøarbeid"
     {
         id: 5,
         iaTjeneste: 2,
         navn: "Utvikle partssamarbeid",
+        deaktivert: true,
     }, {
         id: 6,
         iaTjeneste: 2,
         navn: "Enkel arbeidsmiljøkartlegging",
+        deaktivert: true,
     }, {
         id: 7,
         iaTjeneste: 2,
         navn: "Kontinuerlig (arbeidsmiljø)forbedring",
+        deaktivert: true,
     }, {
         id: 8,
         iaTjeneste: 2,
         navn: "Endring og omstilling",
+        deaktivert: true,
     }, {
         id: 9,
         iaTjeneste: 2,
         navn: "Oppfølging av arbeidsmiljøundersøkelse",
+        deaktivert: true,
     }, {
         id: 10,
         iaTjeneste: 2,
         navn: "Livsfaseorientert personalpolitikk",
+        deaktivert: true,
     },
     {
         id: 14,
         iaTjeneste: 2,
         navn: "Sees i morgen",
+        deaktivert: true,
+    },
+    {
+        id: 16,
+        iaTjeneste: 2,
+        navn: "Forebyggende arbeidsmiljøarbeid",
+        deaktivert: false,
     },
     // 3 - "HelseIArbeid"
     {
         id: 11,
         iaTjeneste: 3,
         navn: "Muskel- og skjelett",
+        deaktivert: true,
     },
     {
         id: 12,
         iaTjeneste: 3,
         navn: "Smertemestring og arbeidsmiljø",
+        deaktivert: true,
     },
     {
         id: 13,
         iaTjeneste: 3,
         navn: "Psykisk helse",
+        deaktivert: true,
+    },
+    {
+        id: 17,
+        iaTjeneste: 3,
+        navn: "HelseIArbeid",
+        deaktivert: false,
     },
 ]
 
--- client/src/domenetyper/leveranse.ts
+++ client/src/domenetyper/leveranse.ts
@@ -3,7 +3,8 @@ import { datoSchema } from "./domenetyper";
 
 export const iaTjenesteSchema = z.object({
     id: z.number(),
-    navn: z.string()
+    navn: z.string(),
+    deaktivert: z.boolean(),
 })
 
 export type IATjeneste = z.infer<typeof iaTjenesteSchema>;
@@ -12,6 +13,7 @@ export const modulSchema = z.object({
     id: z.number(),
     iaTjeneste: z.number(),
     navn: z.string(),
+    deaktivert: z.boolean(),
 })
 
 export type Modul = z.infer<typeof modulSchema>;
@@ -19,7 +21,6 @@ export type Modul = z.infer<typeof modulSchema>;
 const LEVERANSE_STATUSER = [
     "UNDER_ARBEID",
     "LEVERT",
-    // "AVBRUTT"?
 ] as const;
 
 export const LeveranseStatusEnum = z.enum(LEVERANSE_STATUSER);
