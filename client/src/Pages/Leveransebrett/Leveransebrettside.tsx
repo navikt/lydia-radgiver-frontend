fa8a4c80f33c17afd5960ceea0ca76ceef5bf0ff
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-12-04 10:59:20 +0100

    Legg til enkel feilhandtering for henting av mine-leveranser
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>

--- client/src/Pages/Leveransebrett/Leveransebrettside.tsx
+++ client/src/Pages/Leveransebrett/Leveransebrettside.tsx
@@ -1,25 +1,43 @@
-import { Heading } from "@navikt/ds-react";
+import { BodyShort, Heading, Loader } from "@navikt/ds-react";
 import { erIDev } from "../../components/Dekoratør/Dekoratør";
 import { Leveransekort } from "./Leveransekort";
 import { useMineLeveranser } from "../../api/lydia-api";
 
 export const Leveransebrettside = () => {
-	if (!erIDev) {
-		return null;
-	}
+    if (!erIDev) {
+        return null;
+    }
 
-	const { data } = useMineLeveranser(); //TODO: håndtere error og loading for komponent
+    const { data, loading, error } = useMineLeveranser();
 
-	return (
-		<div>
-			<Heading size="large">Leveranser på saker jeg eier</Heading>
-			{
-				data?.map((leveranse, index) => {
-					return (
-						<Leveransekort leveranse={leveranse} key={index} />
-					);
-				})
-			}
-		</div>
-	);
+    if (loading) {
+        return (
+            <div>
+                <Heading size="large">IA-tjenester på saker jeg eier</Heading>
+                <Loader />
+            </div>
+        )
+    }
+    if (error) {
+        return (
+            <div>
+                <Heading size="large">IA-tjenester på saker jeg eier</Heading>
+                <BodyShort>Kunne ikke hente IA-tjenester</BodyShort>
+            </div>
+        )
+    }
+
+    return (
+        <div>
+            <Heading size="large">IA-tjenester på saker jeg eier</Heading>
+            {data?.length ?
+                data.map((leveranse, index) => {
+                    return (
+                        <Leveransekort leveranse={leveranse} key={index} />
+                    );
+                })
+                : <BodyShort>Du har ingen IA-tjenester som er under arbeid</BodyShort>
+            }
+        </div>
+    );
 }
