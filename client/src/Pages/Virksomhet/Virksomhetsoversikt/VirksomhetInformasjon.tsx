91d2d5a9a9384fbb62b9b47257e24b0c926c616e
Ingrid Fosså <fossa.ingrid@gmail.com>
2023-12-12 16:49:26 +0100

    Tek i bruk skildringslister i virksomhetsinformasjon

--- client/src/Pages/Virksomhet/Virksomhetsoversikt/VirksomhetInformasjon.tsx
+++ client/src/Pages/Virksomhet/Virksomhetsoversikt/VirksomhetInformasjon.tsx
@@ -1,37 +1,38 @@
 import styled from "styled-components";
-import {BodyShort} from "@navikt/ds-react";
-import {NavFarger} from "../../../styling/farger";
-import {BorderRadius} from "../../../styling/borderRadius";
-import {Virksomhet} from "../../../domenetyper/virksomhet";
+import { BodyShort } from "@navikt/ds-react";
+import { NavFarger } from "../../../styling/farger";
+import { BorderRadius } from "../../../styling/borderRadius";
+import { Virksomhet } from "../../../domenetyper/virksomhet";
 import { useHentSalesforceUrl } from "../../../api/lydia-api";
 import { EksternLenke } from "../../../components/EksternLenke";
 
 const Container = styled.div`
   flex: 1 20rem;
   height: fit-content;
-  
-  display: grid;
-  grid-template-columns: auto 1fr;
-  grid-template-rows: repeat(auto-fill, auto);
-  row-gap: 0.5rem;
-  column-gap: 1.5rem;
 
   padding: 1.5rem;
   border: 1px solid ${NavFarger.borderMuted};
   border-radius: ${BorderRadius.medium};
 `;
 
-const InfoTittel = styled(BodyShort)`
+const Info = styled.dl`
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-template-rows: repeat(auto-fill, auto);
+  row-gap: 0.5rem;
+  column-gap: 1.5rem;
+`;
+
+const InfoTittel = styled(BodyShort).attrs({ as: "dt" })`
   font-weight: bold;
+`;
+
+const InfoData = styled(BodyShort).attrs({ as: "dd" })`
+  overflow-wrap: anywhere;
 `;
 
 const SalesforceLenke = styled(EksternLenke)`
   padding-top: calc(0.5rem + 1em);
-  grid-column: span 2;
-`;
-
-const InfoData = styled(BodyShort)`
-  overflow-wrap: anywhere;
 `;
 
 interface Props {
@@ -39,9 +40,9 @@ interface Props {
     className?: string;
 }
 
-export const VirksomhetInformasjon = ({virksomhet, className}: Props) => {
+export const VirksomhetInformasjon = ({ virksomhet, className }: Props) => {
     const adresse = virksomhet.adresse.join(', ');
-    const capitalizedLabel = (label :string) => {
+    const capitalizedLabel = (label: string) => {
         const lowerCasedLabel: string = label.toLowerCase()
         return lowerCasedLabel[0].toUpperCase() + lowerCasedLabel.slice(1)
     }
@@ -51,39 +52,42 @@ export const VirksomhetInformasjon = ({virksomhet, className}: Props) => {
     } = useHentSalesforceUrl(virksomhet.orgnr)
 
     return (
-        <Container className={className}>
-            <InfoTittel>Orgnummer</InfoTittel>
-            <InfoData>{virksomhet.orgnr}</InfoData>
-            <InfoTittel>Adresse</InfoTittel>
-            <InfoData>{adresse}, {virksomhet.postnummer} {virksomhet.poststed}</InfoData>
-            {virksomhet.bransje &&
-                <>
-                    <InfoTittel>Bransje</InfoTittel>
-                    <InfoData>{capitalizedLabel(virksomhet.bransje)}</InfoData>
-                </>
-            }
-            <InfoTittel>Næring</InfoTittel>
-            <InfoData>
-                {`${virksomhet.næring.navn} (${virksomhet.næring.kode})`}
-            </InfoData>
-            <InfoTittel>Næringsundergruppe{virksomhet.næringsundergruppe2 && "r"}</InfoTittel>
-            <InfoData>
-                {virksomhet.næringsundergruppe1.navn} ({virksomhet.næringsundergruppe1.kode})
-                {virksomhet.næringsundergruppe2 && `, ${virksomhet.næringsundergruppe2.navn} (${virksomhet.næringsundergruppe2.kode})`}
-                {virksomhet.næringsundergruppe3 && `, ${virksomhet.næringsundergruppe3.navn} (${virksomhet.næringsundergruppe3.kode})`}
-            </InfoData>
-            {virksomhet.sektor &&
-                (<>
-                    <InfoTittel>Sektor</InfoTittel>
-                    <InfoData>{virksomhet.sektor}</InfoData>
-                </>)
-            }
-            {salesforceUrl && (
-                <SalesforceLenke href={salesforceUrl.url}>
+        <Container>
+            <Info className={className}>
+                <InfoTittel>Orgnummer</InfoTittel>
+                <InfoData>{virksomhet.orgnr}</InfoData>
+                <InfoTittel>Adresse</InfoTittel>
+                <InfoData>{adresse}, {virksomhet.postnummer} {virksomhet.poststed}</InfoData>
+                {virksomhet.bransje &&
+                    <>
+                        <InfoTittel>Bransje</InfoTittel>
+                        <InfoData>{capitalizedLabel(virksomhet.bransje)}</InfoData>
+                    </>
+                }
+                <InfoTittel>Næring</InfoTittel>
+                <InfoData>
+                    {`${virksomhet.næring.navn} (${virksomhet.næring.kode})`}
+                </InfoData>
+                <InfoTittel>Næringsundergruppe{virksomhet.næringsundergruppe2 && "r"}</InfoTittel>
+                <InfoData>
+                    {virksomhet.næringsundergruppe1.navn} ({virksomhet.næringsundergruppe1.kode})
+                    {virksomhet.næringsundergruppe2 && `, ${virksomhet.næringsundergruppe2.navn} (${virksomhet.næringsundergruppe2.kode})`}
+                    {virksomhet.næringsundergruppe3 && `, ${virksomhet.næringsundergruppe3.navn} (${virksomhet.næringsundergruppe3.kode})`}
+                </InfoData>
+                {virksomhet.sektor &&
+                    (<>
+                        <InfoTittel>Sektor</InfoTittel>
+                        <InfoData>{virksomhet.sektor}</InfoData>
+                    </>)
+                }
+            </Info>
+            {salesforceUrl &&
+                <SalesforceLenke href={salesforceUrl?.url}>
                     Se virksomheten i Salesforce
                 </SalesforceLenke>
-            )}
+            }
         </Container>
+
     );
 };
 
