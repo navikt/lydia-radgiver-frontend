8e0a625d02aa402ec0cc04e88df23426f88def06
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2023-12-19 12:59:43 +0100

    Fikset på padding i modaler ved høy zoom, så vi får sett noe annet enn "bare" whitespace.
    Co-authored-by: Thomas Dufourd <thomas.dufourd@nav.no>
    Co-authored-by: Per-Christian Nielsen <per-christian.nielsen@nav.no>
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>
    Co-authored-by: Ingrid Fosså <ingrid.fossa@nav.no>

--- client/src/components/Modal/StyledModal.tsx
+++ client/src/components/Modal/StyledModal.tsx
@@ -1,7 +1,14 @@
 import { Modal } from "@navikt/ds-react";
 import styled from "styled-components";
+import { mobileAndUp } from "../../styling/breakpoints";
 
 export const StyledModal = styled(Modal)`
-  padding: 1.5rem;
+  padding: 0rem;
   max-width: 42rem;
+  --a-spacing-6: 0.5rem;
+  
+  ${mobileAndUp} {
+    padding: 1.5rem;
+    --a-spacing-6: var(--a-spacing-6); // Vi prøver å hente ut originalverdien frå designsystemet
+  }
 `;
