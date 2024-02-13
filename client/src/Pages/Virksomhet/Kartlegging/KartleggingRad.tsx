cdf6aff9bdeaf7dd560aaf331aa4d6dff752093c
nimajimale <nima0512@gmail.com>
2024-02-13 17:59:56 +0100

    Statusbadge i KartleggingRad ser ikke lenger ut som lenke on hover

--- client/src/Pages/Virksomhet/Kartlegging/KartleggingRad.tsx
+++ client/src/Pages/Virksomhet/Kartlegging/KartleggingRad.tsx
@@ -1,17 +1,31 @@
-import {IASak} from "../../../domenetyper/domenetyper";
-import {IASakKartlegging} from "../../../domenetyper/iaSakKartlegging";
-import {PågåendeKartleggingRad} from "./PågåendeKartleggingRad";
-import {FullførtKartleggingRad} from "./FullførtKartleggingRad";
-import {KartleggingRadIkkeEier} from "./KartleggingRadIkkeEier";
-import {Accordion} from "@navikt/ds-react";
-import {StatusBadge} from "../../../components/Badge/StatusBadge";
+import { IASak } from "../../../domenetyper/domenetyper";
+import { IASakKartlegging } from "../../../domenetyper/iaSakKartlegging";
+import { PågåendeKartleggingRad } from "./PågåendeKartleggingRad";
+import { FullførtKartleggingRad } from "./FullførtKartleggingRad";
+import { KartleggingRadIkkeEier } from "./KartleggingRadIkkeEier";
+import { Accordion, BodyShort } from "@navikt/ds-react";
+import { StatusBadge } from "../../../components/Badge/StatusBadge";
 import styled from "styled-components";
-import {lokalDatoMedKlokkeslett} from "../../../util/dato";
+import { lokalDatoMedKlokkeslett } from "../../../util/dato";
+import { mobileAndUp } from "../../../styling/breakpoints";
 
 const AccordionHeaderContent = styled.div`
     display: flex;
     align-items: center;
     gap: 2rem;
+`;
+
+const KartleggingStatus = styled(BodyShort)`
+    overflow-wrap: anywhere;
+    margin-bottom: 0.5rem;
+
+    ${mobileAndUp} {
+        margin-bottom: 0;
+    }
+
+    &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+    }
 `;
 
 interface KartleggingRadProps {
@@ -26,14 +40,16 @@ export const KartleggingRad = ({
    brukerErEierAvSak,
 }: KartleggingRadProps) => {
     if (!brukerErEierAvSak) {
-        return <KartleggingRadIkkeEier kartlegging={kartlegging}/>
+        return <KartleggingRadIkkeEier kartlegging={kartlegging} />
     }
 
     return (
         <Accordion.Item>
             <Accordion.Header>
                 <AccordionHeaderContent>
-                    <StatusBadge status={kartlegging.status} />
+                    <KartleggingStatus>
+                        <StatusBadge status={kartlegging.status} />
+                    </KartleggingStatus>
                     Kartlegging opprettet {lokalDatoMedKlokkeslett(kartlegging.opprettetTidspunkt)}
                 </AccordionHeaderContent>
             </Accordion.Header>
