0e07643c8216cd593bfb6cec9a014eee61a707bd
Thomas Dufourd <thomas.dufourd@gmail.com>
2024-05-13 08:40:57 +0200

    Fiks: bruk riktig hostname i åpne kartlegging i ny fane (prod eller preprod/dev)

--- client/src/util/navigasjon.ts
+++ client/src/util/navigasjon.ts
@@ -1,9 +1,14 @@
+import { erIDev } from "../components/Dekoratør/Dekoratør";
+
 export const åpneKartleggingINyFane = (
     kartleggingId: string,
     vertId: string,
     kartleggingStatus: string,
 ) => {
-    const baseUrl = `${import.meta.env.VITE_KARTLEGGING_URL}/${kartleggingId}/vert/${vertId}`;
+    const kartleggingHost = erIDev
+        ? `https://fia-arbeidsgiver.ekstern.dev.nav.no`
+        : `https://fia-arbeidsgiver.nav.no`;
+    const baseUrl = `${kartleggingHost}/${kartleggingId}/vert/${vertId}`;
     switch (kartleggingStatus) {
         case "PÅBEGYNT":
             window.open(`${baseUrl}/oversikt`);
