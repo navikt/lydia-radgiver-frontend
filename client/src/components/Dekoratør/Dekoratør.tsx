509a33aae16d3abcdb03c955feb56bba15f5b8b2
Thomas Dufourd <thomas.dufourd@gmail.com>
2024-06-14 14:01:03 +0200

    Vis Demo banner i demo ingress/løsning
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>
    Co-authored-by: Christian Klem <christian.klem@nav.no>
    Co-authored-by: Sindre Larsen <sindre.larsen@nav.no>

--- client/src/components/Dekoratør/Dekoratør.tsx
+++ client/src/components/Dekoratør/Dekoratør.tsx
@@ -9,142 +9,171 @@ import { mobileAndUp, tabletAndUp } from "../../styling/breakpoints";
 
 // Stylinga her er tatt fra navikt/nav-dekoratøren (15.12.2023)
 const TilHovedinnhold = styled.a`
-  clip: rect(0 0 0 0);
-  border: 0;
-  height: 1px;
-  margin: -1px;
-  overflow: hidden;
-  padding: 0;
-  position: absolute;
-  width: 1px;
-  z-index: 1001;
-
-  &:focus {
-    clip: auto;
-    height: 2.625rem;
-    margin: 0;
-    overflow: visible;
-    padding: 0.8rem;
+    clip: rect(0 0 0 0);
+    border: 0;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
     position: absolute;
-    width: auto;
+    width: 1px;
+    z-index: 1001;
 
-    background-color: var(--a-border-focus);
-    box-shadow: 0 0 0 2px var(--a-border-focus);
-    color: #fff;
-    outline: none;
-    text-decoration: none;
-  }
+    &:focus {
+        clip: auto;
+        height: 2.625rem;
+        margin: 0;
+        overflow: visible;
+        padding: 0.8rem;
+        position: absolute;
+        width: auto;
+
+        background-color: var(--a-border-focus);
+        box-shadow: 0 0 0 2px var(--a-border-focus);
+        color: #fff;
+        outline: none;
+        text-decoration: none;
+    }
 `;
 
 const StyledInternalHeader = styled(InternalHeader)`
-  min-height: unset; // Motvirkar "min-height: 3rem" frå designsystemet som gjer at boksen ikkje får bakgrunn på alt når den wrapper
+    min-height: unset; // Motvirkar "min-height: 3rem" frå designsystemet som gjer at boksen ikkje får bakgrunn på alt når den wrapper
 
-  flex-wrap: wrap;
-  flex-direction: column;
-  
-  ${mobileAndUp} { // Alt utanom pittesmå skjermar
-    flex-direction: row;
-  }
+    flex-wrap: wrap;
+    flex-direction: column;
+
+    ${mobileAndUp} {
+        // Alt utanom pittesmå skjermar
+        flex-direction: row;
+    }
 `;
 
 const Navigasjon = styled.nav`
-  display: flex;
-  flex-direction: column;
-  align-items: start;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
 
-  /* Overskriv border-right når navigasjonslenkene vert vist i kolonne */
-  & .navds-internalheader__title {
-    border-right: none;
-  }
-
-  ${tabletAndUp} {
-    flex-direction: row;
-    align-items: center;
-
+    /* Overskriv border-right når navigasjonslenkene vert vist i kolonne */
     & .navds-internalheader__title {
-      /* Stylinga vi overskriv frå designsystemet (2023-12-21) */
-      border-right: 1px solid var(--ac-internalheader-divider, var(--a-gray-600));
+        border-right: none;
     }
-  }
+
+    ${tabletAndUp} {
+        flex-direction: row;
+        align-items: center;
+
+        & .navds-internalheader__title {
+            /* Stylinga vi overskriv frå designsystemet (2023-12-21) */
+            border-right: 1px solid
+                var(--ac-internalheader-divider, var(--a-gray-600));
+        }
+    }
 `;
 
 const Navigasjonslenke = styled(Link)`
-  color: ${NavFarger.textInverted};
-  text-decoration: none;
+    color: ${NavFarger.textInverted};
+    text-decoration: none;
 
-  &:hover {
-    text-decoration: underline;
-  }
+    &:hover {
+        text-decoration: underline;
+    }
 `;
 
 const DemoversjonTekst = styled(BodyShort)<{ hidden: boolean }>`
-  display: ${(props) => props.hidden ? "none" : "flex"};
-  justify-content: center;
-  align-items: center;
+    display: ${(props) => (props.hidden ? "none" : "flex")};
+    justify-content: center;
+    align-items: center;
 
-  padding: 0 1.5rem;
+    padding: 0 1.5rem;
 
-  color: ${NavFarger.white};
-  background: ${NavFarger.red500};
+    color: ${NavFarger.white};
+    background: ${NavFarger.red500};
 `;
 
 const SøkOgBrukerinfoContainer = styled.div`
-  flex: 1;
+    flex: 1;
 
-  display: flex;
-  flex-direction: column;
-  
-  ${mobileAndUp} {
-    flex-direction: row;
-  }
+    display: flex;
+    flex-direction: column;
+
+    ${mobileAndUp} {
+        flex-direction: row;
+    }
 `;
 
 const Virksomhetssøk = styled(Søkefelt)`
-  min-width: 16rem;
-  width: 25%;
+    min-width: 16rem;
+    width: 25%;
 
-  align-self: center;
+    align-self: center;
 
-  /* Sentrerer søkeboks i det ledig området */
-  margin-left: auto;
-  margin-right: auto;
+    /* Sentrerer søkeboks i det ledig området */
+    margin-left: auto;
+    margin-right: auto;
 `;
 
 const Brukerinformasjon = styled(InternalHeader.User)`
-  margin-left: auto;
-  color: ${NavFarger.white}
+    margin-left: auto;
+    color: ${NavFarger.white};
 `;
 
-export const erIDev = ["localhost", "fia.intern.dev.nav.no", "fia.ekstern.dev.nav.no"].includes(window.location.hostname)
+export const erIDev = [
+    "localhost",
+    "fia.intern.dev.nav.no",
+    "fia.ekstern.dev.nav.no",
+    "fia.ansatt.dev.nav.no",
+].includes(window.location.hostname);
+export const erIDemo = ["demo-fia.ansatt.dev.nav.no"].includes(
+    window.location.hostname,
+);
 
 interface Props {
     brukerInformasjon: BrukerinformasjonType;
 }
+
+const visDemoBanner = erIDemo || erIDev;
 
 export const Dekoratør = ({ brukerInformasjon }: Props) => {
     return (
         <>
             <StyledInternalHeader className="w-full" data-theme="light">
                 <Navigasjon>
-                    <TilHovedinnhold href="#maincontent">Hopp til hovedinnhold</TilHovedinnhold>
+                    <TilHovedinnhold href="#maincontent">
+                        Hopp til hovedinnhold
+                    </TilHovedinnhold>
                     <Navigasjonslenke href="/" title="Gå til søkesiden">
                         <InternalHeader.Title as="h1">Fia</InternalHeader.Title>
                     </Navigasjonslenke>
-                    <Navigasjonslenke href="/statusoversikt" title="Gå til statusoversiktsiden">
-                        <InternalHeader.Title as="span">Statusoversikt</InternalHeader.Title>
+                    <Navigasjonslenke
+                        href="/statusoversikt"
+                        title="Gå til statusoversiktsiden"
+                    >
+                        <InternalHeader.Title as="span">
+                            Statusoversikt
+                        </InternalHeader.Title>
                     </Navigasjonslenke>
-                    <Navigasjonslenke href="/iatjenesteoversikt" title="Gå til mine IA-tjenester">
-                        <InternalHeader.Title as="span">Mine IA-tjenester</InternalHeader.Title>
+                    <Navigasjonslenke
+                        href="/iatjenesteoversikt"
+                        title="Gå til mine IA-tjenester"
+                    >
+                        <InternalHeader.Title as="span">
+                            Mine IA-tjenester
+                        </InternalHeader.Title>
                     </Navigasjonslenke>
                 </Navigasjon>
-                <DemoversjonTekst hidden={!erIDev}>Demoutgave</DemoversjonTekst>
+                <DemoversjonTekst hidden={!visDemoBanner}>
+                    Demoutgave
+                </DemoversjonTekst>
                 <SøkOgBrukerinfoContainer>
                     <Virksomhetssøk />
-                    <Brukerinformasjon name={brukerInformasjon.navn} description={brukerInformasjon.ident} />
+                    <Brukerinformasjon
+                        name={brukerInformasjon.navn}
+                        description={brukerInformasjon.ident}
+                    />
                 </SøkOgBrukerinfoContainer>
             </StyledInternalHeader>
             <SesjonBanner tokenUtløper={brukerInformasjon.tokenUtløper} />
             <NyStatistikkPubliseresBanner />
         </>
-    )
-}
+    );
+};
