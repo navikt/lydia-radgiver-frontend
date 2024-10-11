788593a58084e1eedd472bcfe100981b22b7d97f
Sindre Larsen <silar.dev@proton.me>
2024-10-11 10:44:45 +0200

    Oppdaterer http-proxy-middleware

--- server/proxy.ts
+++ server/proxy.ts
@@ -1,44 +1,52 @@
 import { Request, Response } from "express";
 import { ClientRequest } from "http";
-import { createProxyMiddleware, Options } from "http-proxy-middleware"
+import { createProxyMiddleware, Options } from "http-proxy-middleware";
 
 import { Config } from "./config";
 
 export class LydiaApiProxy {
-    options : Options
-    constructor(config: Config) {
-        const whitelistedPaths = [
-            '/sykefravarsstatistikk',
-            '/iasak/radgiver',
-            '/virksomhet',
-            '/statusoversikt',
-            '/iatjenesteoversikt',
-            '/iasak/minesaker',
-            `/iasak/team`,
-        ]
+  options: Options;
+  constructor(config: Config) {
+    const whitelistedPaths = [
+      "/sykefravarsstatistikk",
+      "/iasak/radgiver",
+      "/virksomhet",
+      "/statusoversikt",
+      "/iatjenesteoversikt",
+      "/iasak/minesaker",
+      `/iasak/team`,
+    ];
 
-        this.options = {
-            target: config.lydiaApi.uri,
-            changeOrigin: true,
-            pathRewrite: (path : string) => {
-                return path.replace("/api", '');
-            },
-            onProxyReq: (proxyReq : ClientRequest, _req: Request, res: Response) => {
-                if (whitelistedPaths.filter(whitelistedPath => proxyReq.path.startsWith(whitelistedPath)).length > 0) {
-                    proxyReq.setHeader('Authorization', `Bearer ${res.locals.on_behalf_of_token}`)
-                    proxyReq.setHeader("x-request-id", res.locals.requestId)
-                } else {
-                    res.sendStatus(404)
-                }
-            },
-        }
-    }
-    createExpressMiddleWare() {
-        return createProxyMiddleware(this.options)
-    }
+    this.options = {
+      target: config.lydiaApi.uri,
+      changeOrigin: true,
+      pathRewrite: (path: string) => {
+        return path.replace("/api", "");
+      },
+      on: {
+        proxyReq: (
+          clientRequest: ClientRequest,
+          _req: Request,
+          res: Response,
+        ) => {
+          if (
+            whitelistedPaths.filter((whitelistedPath) =>
+              clientRequest.path.startsWith(whitelistedPath),
+            ).length > 0
+          ) {
+            clientRequest.setHeader(
+              "Authorization",
+              `Bearer ${res.locals.on_behalf_of_token}`,
+            );
+            clientRequest.setHeader("x-request-id", res.locals.requestId);
+          } else {
+            res.sendStatus(404);
+          }
+        },
+      },
+    };
+  }
+  createExpressMiddleWare() {
+    return createProxyMiddleware(this.options);
+  }
 }
-
-
-
-
-
