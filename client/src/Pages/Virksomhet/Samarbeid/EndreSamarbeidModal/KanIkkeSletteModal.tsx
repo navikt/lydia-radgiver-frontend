d43d277fb513772590d67adbc4ea73ecef53c141
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2025-03-18 12:25:00 +0100

    La til modalen for å vise hvorfor man ikke kan slette et samarbeid (ikke i bruk enda).

---
+++ client/src/Pages/Virksomhet/Samarbeid/EndreSamarbeidModal/KanIkkeSletteModal.tsx
@@ -0,0 +1,75 @@
+import { Alert, BodyLong, Heading, List, Modal } from "@navikt/ds-react";
+import { IaSakProsess } from "../../../../domenetyper/iaSakProsess";
+import { EksternLenke } from "../../../../components/EksternLenke";
+import React from "react";
+import styled from "styled-components";
+import { useHentSalesforceUrl } from "../../../../api/lydia-api/virksomhet";
+import { useVirksomhetContext } from "../../VirksomhetContext";
+
+export default function BekreftSlettingModal({
+	åpen,
+	lukkModal,
+	samarbeid,
+	begrunnelser,
+}: {
+	åpen: boolean;
+	lukkModal: () => void;
+	samarbeid: IaSakProsess;
+	begrunnelser: string[];
+}) {
+	const prettyBegrunnelser = usePrettyBegrunnelser(begrunnelser);
+	const { virksomhet } = useVirksomhetContext();
+	const { data: salesforceInfo } = useHentSalesforceUrl(virksomhet.orgnr);
+
+	return (
+		<Modal open={åpen} onClose={lukkModal} aria-labelledby="bekreft-slett-samarbeid-modal-heading" closeOnBackdropClick>
+			<Modal.Header>
+				<Heading size="medium" id="bekreft-slett-samarbeid-modal-heading">Slett <i>Avdeling</i> {samarbeid.navn}</Heading>
+			</Modal.Header>
+			<Modal.Body>
+				<BodyLong spacing>
+					Samarbeid med fullførte behovsvurderinger, evalueringer og aktive planer kan ikke slettes. Aktiviteter i Salesforce må slettes eller flyttes til at annet samarbeid.
+				</BodyLong>
+				<Alert variant="warning">
+					<Heading spacing size="small" level="3">
+						Samarbeidet kan ikke slettes:
+					</Heading>
+					<List>
+						{
+							prettyBegrunnelser.map((begrunnelse) => (
+								<List.Item key={begrunnelse}>{begrunnelse}</List.Item>
+							))
+						}
+					</List>
+				</Alert>
+				{salesforceInfo && (
+					<SalesforceLenke href={salesforceInfo?.url}>
+						Se virksomhet i Salesforce
+					</SalesforceLenke>
+				)}
+			</Modal.Body>
+		</Modal>
+	);
+}
+
+const SalesforceLenke = styled(EksternLenke)`
+    font-size: 1.125rem;
+	margin-top: 3rem;
+`;
+
+function usePrettyBegrunnelser(begrunnelser: string[]): string[] {
+	return React.useMemo(() => begrunnelser.map((begrunnelse) => {
+		switch (begrunnelse) {
+			case "SALESFORCE_ACTIVITET":
+				return "Aktiviteter i Salesforce";
+			case "FULLFØRT_BEHOVSVURDERING":
+				return "Fullført behovsvurdering";
+			case "AKTIV_SAMARBEIDSPLAN":
+				return "Aktiv samarbeidsplan";
+			case "PÅBEGYNT_EVALUERING":
+				return "Påbegynt evaluering";
+			default:
+				return begrunnelse;
+		}
+	}), [begrunnelser]);
+}
---
+++ client/src/Pages/Virksomhet/Samarbeid/EndreSamarbeidModal/index.tsx
@@ -0,0 +1,267 @@
+import {
+    DEFAULT_SAMARBEIDSNAVN,
+    defaultNavnHvisTomt,
+    IaSakProsess,
+} from "../../../../domenetyper/iaSakProsess";
+import {
+    Alert,
+    BodyShort,
+    Button,
+    Detail,
+    Heading,
+    Link,
+    Modal,
+    TextField,
+} from "@navikt/ds-react";
+import { IASak, IASakshendelseType } from "../../../../domenetyper/domenetyper";
+import React, { useEffect, useState } from "react";
+import {
+    useHentAktivSakForVirksomhet,
+    useHentSamarbeidshistorikk,
+} from "../../../../api/lydia-api/virksomhet";
+import { nyHendelsePåSak } from "../../../../api/lydia-api/sak";
+import styled from "styled-components";
+import { ExternalLinkIcon, TrashIcon } from "@navikt/aksel-icons";
+import { useHentSamarbeid } from "../../../../api/lydia-api/spørreundersøkelse";
+import { StyledSamarbeidModal } from "../NyttSamarbeidModal";
+/* import KanIkkeSletteModal from "./KanIkkeSletteModal"; */
+
+export const ModalBodyInnholdFlex = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+`;
+
+export const TextFieldStyled = styled(TextField)`
+    min-width: fit-content;
+    width: 100%;
+`;
+
+export const DetaljerWrapper = styled.div<{ $disabled?: boolean }>`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 0.5rem;
+    opacity: ${({ $disabled }) => ($disabled ? 0.25 : 1)};
+`;
+
+const SquareButton = styled(Button)`
+    width: 3rem;
+    height: 3rem;
+`;
+
+interface EndreSamarbeidModalProps {
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    samarbeid: IaSakProsess;
+    iaSak: IASak;
+}
+
+export const MAX_LENGDE_SAMARBEIDSNAVN = 50;
+
+export const EndreSamarbeidModal = ({
+    open,
+    setOpen,
+    samarbeid,
+    iaSak,
+}: EndreSamarbeidModalProps) => {
+    const [antallTegn, setAntallTegn] = useState(samarbeid.navn?.length ?? 0);
+    const [navn, setNavn] = useState(defaultNavnHvisTomt(samarbeid.navn));
+    const [lagreNavnVellykket, setLagreNavnVellykket] = useState(false);
+    /* const [slettevarselÅpen, setSlettevarselÅpen] = useState(false);
+    const sletteresultat = {
+        kanSlettes: false,
+        begrunnelser: [
+            "SALESFORCE_ACTIVITET",
+            "FULLFØRT_BEHOVSVURDERING",
+            "AKTIV_SAMARBEIDSPLAN",
+            "PÅBEGYNT_EVALUERING",
+        ]
+    } */
+
+    useEffect(() => {
+        setNavn(defaultNavnHvisTomt(samarbeid.navn));
+        setAntallTegn(defaultNavnHvisTomt(samarbeid.navn)?.length ?? 0);
+    }, [samarbeid]);
+
+    useEffect(() => {
+        setLagreNavnVellykket(false);
+    }, [open]);
+
+    const { mutate: mutateSamarbeidshistorikk } = useHentSamarbeidshistorikk(
+        iaSak.orgnr,
+    );
+    const { mutate: mutateHentSaker } = useHentAktivSakForVirksomhet(
+        iaSak.orgnr,
+    );
+    const { mutate: hentSamarbeidPåNytt, data: samarbeidData } =
+        useHentSamarbeid(iaSak.orgnr, iaSak.saksnummer);
+
+    const navnErUbrukt =
+        samarbeidData?.find(
+            (s) =>
+                s.navn?.toLowerCase() === navn.toLowerCase() ||
+                (navn.toLowerCase() === DEFAULT_SAMARBEIDSNAVN.toLowerCase() &&
+                    s.navn === ""),
+        ) === undefined;
+
+    const avbrytEndring = () => {
+        setOpen(false);
+        setLagreNavnVellykket(false);
+        hentSamarbeidPåNytt().then(() => {
+            setNavn(defaultNavnHvisTomt(samarbeid.navn));
+        });
+    };
+
+    const slettSamarbeid = () => {
+        //TODO: sjekk om sletting er tillatt, og evt. åpne slettevarsel.
+        // setSlettevarselÅpen(true);
+        nyHendelse("SLETT_PROSESS").then(() => {
+            setOpen(false);
+        });
+    };
+
+    const endreNavn = () => {
+        nyHendelse("ENDRE_PROSESS").then(() => {
+            setOpen(false);
+        });
+    };
+
+    const nyHendelse = (hendelsestype: IASakshendelseType) => {
+        const nyttNavn = navn.trim();
+        return nyHendelsePåSak(
+            iaSak,
+            {
+                saksHendelsestype: hendelsestype,
+                gyldigeÅrsaker: [],
+            },
+            null,
+            {
+                ...samarbeid,
+                navn: nyttNavn,
+            },
+        ).then(() => {
+            mutateHentSaker();
+            mutateSamarbeidshistorikk();
+            hentSamarbeidPåNytt().then(() => {
+                setLagreNavnVellykket(true);
+            });
+        });
+    };
+
+    return (
+        <>
+            <StyledSamarbeidModal
+                open={open}
+                onClose={() => {
+                    avbrytEndring();
+                }}
+                width={"small"}
+                aria-label={"Administrer samarbeid"}
+            >
+                <Modal.Header closeButton={true}>
+                    <Heading size="medium">Administrer samarbeid</Heading>
+                </Modal.Header>
+                <Modal.Body>
+                    <ModalBodyInnholdFlex>
+                        <BodyShort>
+                            Her kan du endre navn på samarbeidet &quot;
+                            {defaultNavnHvisTomt(samarbeid.navn)}&quot;
+                            Samarbeidsnavn skal beskrive den avdelingen eller
+                            gruppen man samarbeider med. Navnet må være det samme
+                            som virksomheten bruker selv.
+                        </BodyShort>
+                        <br />
+                        <TextFieldStyled
+                            maxLength={MAX_LENGDE_SAMARBEIDSNAVN}
+                            size="small"
+                            label="Navngi samarbeid"
+                            value={navn}
+                            onChange={(nyttnavn) => {
+                                setNavn(nyttnavn.target.value);
+                                setLagreNavnVellykket(false);
+                                setAntallTegn(nyttnavn.target.value.length);
+                            }}
+                            error={
+                                navnErUbrukt || samarbeid.navn === navn
+                                    ? undefined
+                                    : "Navnet er allerede i bruk"
+                            }
+                            onKeyDown={(event) => {
+                                // Submit på enter.
+                                if (event.key === "Enter" && navnErUbrukt) {
+                                    endreNavn();
+                                }
+                            }}
+                            hideLabel
+                        />
+                        <DetaljerWrapper>
+                            <Detail>
+                                <b>
+                                    Husk, aldri skriv{" "}
+                                    <Link
+                                        href="https://www.datatilsynet.no/rettigheter-og-plikter/personopplysninger/"
+                                        inlineText
+                                    >
+                                        personopplysninger
+                                        <ExternalLinkIcon aria-hidden />
+                                    </Link>
+                                    .
+                                </b>
+                            </Detail>
+                            <Detail>
+                                {antallTegn}/{MAX_LENGDE_SAMARBEIDSNAVN} tegn
+                            </Detail>
+                        </DetaljerWrapper>
+                    </ModalBodyInnholdFlex>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        variant="primary"
+                        onClick={() => {
+                            endreNavn();
+                        }}
+                        disabled={
+                            !navnErUbrukt ||
+                            navn === defaultNavnHvisTomt(samarbeid.navn)
+                        }
+                    >
+                        Lagre
+                    </Button>
+                    <Button
+                        variant="secondary"
+                        onClick={() => {
+                            avbrytEndring();
+                        }}
+                    >
+                        Avbryt
+                    </Button>
+                    <SquareButton
+                        icon={
+                            <TrashIcon
+                                title={`Slett "${samarbeid.navn}"`}
+                                fontSize="2rem"
+                            />
+                        }
+                        size={"small"}
+                        variant="secondary-neutral"
+                        title={`Slett "${samarbeid.navn}"`}
+                        onClick={slettSamarbeid}
+                    />
+                    {lagreNavnVellykket && (
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                            <Alert inline variant="success" size="small">
+                                Lagret
+                            </Alert>
+                        </div>
+                    )}
+                </Modal.Footer>
+            </StyledSamarbeidModal>
+            {/* <KanIkkeSletteModal
+                åpen={slettevarselÅpen}
+                lukkModal={() => setSlettevarselÅpen(false)}
+                samarbeid={samarbeid}
+                begrunnelser={sletteresultat.begrunnelser} /> */}
+        </>
+    );
+};
