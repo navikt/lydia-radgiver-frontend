1cd337d90d025d5d59ea87dad9aabfb23aac5951
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2025-05-23 11:59:23 +0200

    Krympet innhold i graf tooltip for sykefraværsstatistikk.

--- client/src/Pages/Virksomhet/Statistikk/Graf/GraphTooltip.tsx
+++ client/src/Pages/Virksomhet/Statistikk/Graf/GraphTooltip.tsx
@@ -37,30 +37,28 @@ const Ikon = styled(SymbolSvg)`
     margin-right: 0.5rem;
 `;
 
-export const graphTooltip = () => {
+export function graphTooltip() {
     return (
         <Tooltip
             itemSorter={(item) => graflinjer[item.dataKey as Grafer].rekkefølge}
             formatter={(value: number, name: string) => [
                 <TooltipItemWrapper key={`tooltip-${name}`}>
                     <Ikon
-                        size={40}
+                        size={30}
                         fill={graflinjer[name].farge}
-                        symbol={graflinjer[name].symbol}
-                    />
+                        symbol={graflinjer[name].symbol} />
                     <NavnOgVerdi>
                         <Navn>{graflinjer[name].navn}</Navn>
                         <Verdi>{value + " %"}</Verdi>
                     </NavnOgVerdi>
                 </TooltipItemWrapper>,
             ]}
-            separator={": "}
             contentStyle={{
                 border: "2px solid #254B6D",
                 borderRadius: "0.25rem",
             }}
             labelStyle={{ paddingBottom: "0.5rem" }}
-            cursor={{ stroke: "#254B6D", strokeWidth: 3, type: "dot" }}
-        />
+            itemStyle={{ paddingTop: 0, paddingBottom: 0 }}
+            cursor={{ stroke: "#254B6D", strokeWidth: 3, type: "dot" }} />
     );
-};
+}
