20f68e8e5adc8cd9a338ff99d348a80a3b7c2203
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2025-09-30 12:09:35 +0200

    Flyttet sakshistorikk inn i ny komponent i forberedelse av å putte i modal.

--- client/src/Pages/Virksomhet/Sakshistorikk/SakshistorikkFane.tsx
+++ client/src/Pages/Virksomhet/Sakshistorikk/SakshistorikkFane.tsx
@@ -1,99 +1,13 @@
-import { Accordion, BodyShort, Heading, Loader } from "@navikt/ds-react";
-import { lokalDato } from "../../../util/dato";
-import { IAProsessStatusBadge } from "../../../components/Badge/IAProsessStatusBadge";
-import { SakshistorikkTabell } from "./SakshistorikkTabell";
-import { Sakshistorikk } from "../../../domenetyper/sakshistorikk";
-import { useHentSakshistorikk } from "../../../api/lydia-api/virksomhet";
-import { LeveransehistorikkTabell } from "./LeveransehistorikkTabell";
-import Samarbeidshistorikk from "./Samarbeidshistorikk";
-import styles from './sakshistorikk.module.scss';
+import SakshistorikkMedDatahenting from "./SakshistorikkInnhold";
 
 interface SakshistorikkProps {
     orgnr: string;
-    className?: string;
 }
 
 export const SakshistorikkFane = ({
     orgnr,
-    className,
 }: SakshistorikkProps) => {
-    const { data: sakshistorikk, loading: lasterSakshistorikk } =
-        useHentSakshistorikk(orgnr);
-
-    if (lasterSakshistorikk) {
-        return (
-            <div className={`${styles.samarbeidshistorikkfaneContainer} ${className}`}>
-                <Heading spacing={true} size="large">
-                    Sakshistorikk
-                </Heading>
-                <Loader />
-            </div>
-        );
-    }
-
-    if (!sakshistorikk) {
-        return (
-            <div className={`${styles.samarbeidshistorikkfaneContainer} ${className}`}>
-                <Heading spacing={true} size="large">
-                    Sakshistorikk
-                </Heading>
-                <BodyShort>Kunne ikke hente sakshistorikk</BodyShort>
-            </div>
-        );
-    }
-
-    const sortertHistorikk = sakshistorikk.map((historikk) => ({
-        ...historikk,
-        sakshendelser: sorterSakshistorikkPåTid(historikk),
-    }));
-
     return (
-        <div className={`${styles.samarbeidshistorikkfaneContainer} ${className}`}>
-            <Heading level="3" size="large" spacing={true}>
-                Historikk
-            </Heading>
-            {sortertHistorikk.length > 0 ? (
-                <Accordion>
-                    {sortertHistorikk.map((sakshistorikk) => (
-                        <Accordion.Item key={sakshistorikk.saksnummer}>
-                            <Accordion.Header>
-                                <div className={styles.accordionHeaderContent}>
-                                    <IAProsessStatusBadge
-                                        status={
-                                            sakshistorikk.sakshendelser[0]
-                                                .status
-                                        }
-                                    />
-                                    Sist oppdatert:{" "}
-                                    {lokalDato(sakshistorikk.sistEndret)}
-                                </div>
-                            </Accordion.Header>
-                            <Accordion.Content>
-                                <Samarbeidshistorikk historikk={sakshistorikk} orgnr={orgnr} />
-                                <LeveransehistorikkTabell
-                                    orgnr={orgnr}
-                                    saksnummer={sakshistorikk.saksnummer}
-                                />
-                                <SakshistorikkTabell
-                                    key={sakshistorikk.saksnummer}
-                                    sakshistorikk={sakshistorikk}
-                                />
-                            </Accordion.Content>
-                        </Accordion.Item>
-                    ))}
-                </Accordion>
-            ) : (
-                <BodyShort>
-                    Fant ingen sakshistorikk på denne virksomheten
-                </BodyShort>
-            )}
-        </div>
-    );
+        <SakshistorikkMedDatahenting orgnr={orgnr} />
+    )
 };
-
-export function sorterSakshistorikkPåTid({ sakshendelser }: Sakshistorikk) {
-    return sakshendelser.sort(
-        (a, b) =>
-            b.tidspunktForSnapshot.getTime() - a.tidspunktForSnapshot.getTime(),
-    );
-}
---
+++ client/src/Pages/Virksomhet/Sakshistorikk/SakshistorikkInnhold/index.tsx
@@ -0,0 +1,96 @@
+import { Accordion, BodyShort, Heading, Loader } from "@navikt/ds-react";
+import { useHentSakshistorikk } from "../../../../api/lydia-api/virksomhet";
+import { Sakshistorikk } from "../../../../domenetyper/sakshistorikk";
+import styles from './sykefraværshistorikkinnhold.module.scss';
+import { IAProsessStatusBadge } from "../../../../components/Badge/IAProsessStatusBadge";
+import { lokalDato } from "../../../../util/dato";
+import Samarbeidshistorikk from "../Samarbeidshistorikk";
+import { LeveransehistorikkTabell } from "../LeveransehistorikkTabell";
+import { SakshistorikkTabell } from "../SakshistorikkTabell";
+
+type SakshistorikkInnholdProps = {
+	sakshistorikk?: Sakshistorikk[],
+	lasterSakshistorikk: boolean,
+	orgnr: string,
+}
+
+export default function SakshistorikkMedDatahenting({ orgnr, Innhold = SakshistorikkWrapper }: { orgnr: string, Innhold?: React.ComponentType<SakshistorikkInnholdProps> }) {
+	const { data: sakshistorikk, loading: lasterSakshistorikk } = useHentSakshistorikk(orgnr);
+
+	return <Innhold sakshistorikk={sakshistorikk} lasterSakshistorikk={lasterSakshistorikk} orgnr={orgnr} />;
+}
+
+function SakshistorikkWrapper({ sakshistorikk, lasterSakshistorikk, orgnr }: SakshistorikkInnholdProps) {
+	return (
+		<div className={styles.samarbeidshistorikkfaneContainer}>
+			<Heading level="3" size="large" spacing={true}>
+				Historikk
+			</Heading>
+			<SykefraværshistorikkInnhold sakshistorikk={sakshistorikk} lasterSakshistorikk={lasterSakshistorikk} orgnr={orgnr} />
+		</div>
+	)
+}
+
+function SykefraværshistorikkInnhold({ sakshistorikk, lasterSakshistorikk, orgnr }: SakshistorikkInnholdProps) {
+	if (lasterSakshistorikk) {
+		return (
+			<Loader />
+		);
+	}
+
+	if (!sakshistorikk) {
+		return (
+			<BodyShort>Kunne ikke hente sakshistorikk</BodyShort>
+		);
+	}
+
+	if (sakshistorikk.length === 0) {
+		return (
+			<BodyShort>Fant ingen sakshistorikk på denne virksomheten</BodyShort>
+		);
+	}
+
+	const sortertHistorikk = sakshistorikk.map((historikk) => ({
+		...historikk,
+		sakshendelser: sorterSakshistorikkPåTid(historikk),
+	}));
+
+	return (
+		<Accordion>
+			{sortertHistorikk.map((sakshistorikk) => (
+				<Accordion.Item key={sakshistorikk.saksnummer}>
+					<Accordion.Header>
+						<div className={styles.accordionHeaderContent}>
+							<IAProsessStatusBadge
+								status={
+									sakshistorikk.sakshendelser[0]
+										.status
+								}
+							/>
+							Sist oppdatert:{" "}
+							{lokalDato(sakshistorikk.sistEndret)}
+						</div>
+					</Accordion.Header>
+					<Accordion.Content>
+						<Samarbeidshistorikk historikk={sakshistorikk} orgnr={orgnr} />
+						<LeveransehistorikkTabell
+							orgnr={orgnr}
+							saksnummer={sakshistorikk.saksnummer}
+						/>
+						<SakshistorikkTabell
+							key={sakshistorikk.saksnummer}
+							sakshistorikk={sakshistorikk}
+						/>
+					</Accordion.Content>
+				</Accordion.Item>
+			))}
+		</Accordion>
+	)
+}
+
+function sorterSakshistorikkPåTid({ sakshendelser }: Sakshistorikk) {
+	return sakshendelser.sort(
+		(a, b) =>
+			b.tidspunktForSnapshot.getTime() - a.tidspunktForSnapshot.getTime(),
+	);
+}
