2a1eb64a1f32ed0400787b3fdb9b89c126880acd
Sverre Blekastad Stensby <sverre.blekastad.stensby@nav.no>
2025-10-24 14:50:42 +0200

    Tvinger TextEncoder og TextDecoder inn i jest setup.

--- client/jest.setup.ts
+++ client/jest.setup.ts
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { toHaveNoViolations } from "jest-axe";
+import { TextEncoder, TextDecoder } from "util";
 
 expect.extend(toHaveNoViolations);
+// @ts-ignore
+global.TextEncoder = TextEncoder;
+// @ts-ignore
+global.TextDecoder = TextDecoder;
