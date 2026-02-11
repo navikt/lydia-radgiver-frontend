f106f6fc9ea57875b6ed192a8842046c037cf40a
Christian Klem <christian.klem@nav.no>
2026-02-11 15:32:51 +0100

    Legg til modal for nedlastning av kartleggingsresultater
    Co-authored-by: Nima Jimale <nima.jimale@nav.no>

--- client/src/Pages/Virksomhet/Kartlegging/ResultatEksportVisning.tsx
+++ client/src/Pages/Virksomhet/Kartlegging/ResultatEksportVisning.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@navikt/ds-react";
-import React from "react";
+import { BodyShort, Button, Detail, Modal } from "@navikt/ds-react";
+import React, { useState } from "react";
 import { FilePdfIcon } from "@navikt/aksel-icons";
 import { IASak } from "../../../domenetyper/domenetyper";
 import { Spørreundersøkelse } from "../../../domenetyper/spørreundersøkelse";
@@ -18,22 +18,59 @@ const ResultatEksportVisning = ({
     if (spørreundersøkelse.status !== "AVSLUTTET") {
         return null;
     }
+    const [åpen, setÅpen] = useState(false);
 
     return (
         <>
+            <Modal
+                aria-label="Last ned kartleggingsresultater som pdf"
+                open={åpen}
+                onClose={() => setÅpen(false)}
+                header={{ heading: "Last ned kartleggingsresultater som pdf" }}
+            >
+                <Modal.Body>
+                    <BodyShort>
+                        Her kan du laste ned resultatene av kartleggingen for
+                        deling videre til arbeidsgiver.
+                    </BodyShort>
+                    <br />
+                    <Detail>
+                        Merk: Dokumentet blir journalført som et utgående
+                        dokument i Gosys.
+                    </Detail>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        as="a"
+                        href={kartleggingresultatPdfLenke(
+                            iaSak.orgnr,
+                            iaSak.saksnummer,
+                            spørreundersøkelse.id,
+                        )}
+                        onClick={() => {
+                            setÅpen(false);
+                        }}
+                    >
+                        Last ned
+                    </Button>
+                    <Button
+                        variant="secondary"
+                        onClick={() => {
+                            setÅpen(false);
+                        }}
+                    >
+                        Avbryt
+                    </Button>
+                </Modal.Footer>
+            </Modal>
             <Button
-                as="a"
-                href={kartleggingresultatPdfLenke(
-                    iaSak.orgnr,
-                    iaSak.saksnummer,
-                    spørreundersøkelse.id,
-                )}
                 icon={<FilePdfIcon fontSize="1.5rem" aria-hidden />}
                 iconPosition="right"
                 variant="secondary"
                 size="small"
                 onClick={() => {
                     loggEksportertTilPdf("kartlegging");
+                    setÅpen(true);
                 }}
             >
                 Last ned
