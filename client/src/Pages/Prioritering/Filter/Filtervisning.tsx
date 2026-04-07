5f90d412ac0a065ebf02604b1aee81b71c0f9d73
Christian Klem <christian.klem@nav.no>
2026-04-07 12:26:56 +0200

    Fiks linting

--- client/src/Pages/Prioritering/Filter/Filtervisning.tsx
+++ client/src/Pages/Prioritering/Filter/Filtervisning.tsx
@@ -50,7 +50,6 @@ export const Filtervisning = ({
     const {
         oppdaterAntallArbeidsforhold,
         oppdaterVirksomhetTilstand,
-        oppdaterIastatus,
         oppdaterEiere,
         oppdaterFylker,
         state,
