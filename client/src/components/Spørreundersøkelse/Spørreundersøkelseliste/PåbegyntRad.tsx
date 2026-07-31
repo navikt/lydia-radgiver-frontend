dee03806238f8f452f0de2e92a2f6aec5a73eec4
Thomas Dufourd <thomas.dufourd@gmail.com>
2026-07-31 08:45:49 +0200

    Bruk nytt 'fullfør kartlegging' endepunkt POST /proxy/api/v1/virksomhet/${orgnummer}/samarbeidsperiode/${saksnummer}/samarbeid/${samarbeidId}/kartlegging/${kartleggingId}/fullfor

--- client/src/components/Spørreundersøkelse/Spørreundersøkelseliste/PåbegyntRad.tsx
+++ client/src/components/Spørreundersøkelse/Spørreundersøkelseliste/PåbegyntRad.tsx
@@ -56,6 +56,7 @@ export default function PåbegyntRad({
     const fullførSpørreundersøkelse = () => {
         fullførKartleggingNyFlyt(
             iaSak.orgnr,
+            iaSak.saksnummer,
             samarbeidsId,
             spørreundersøkelse.id,
         ).then(() => {
